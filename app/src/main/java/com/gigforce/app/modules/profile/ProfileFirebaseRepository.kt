c6a1f25c5689de1f5068720730f417def306795f
1995YogeshSharma <yogesh.sharma@research.iiit.ac.in>
2020-02-25 13:23:51 +0530

    Directly use object to setValue in firebase
    Earlier, the code was constructing a map from object to set value.
    After reading documentation, I realized we don't need to do that
    and can directly upload object.

--- app/src/main/java/com/gigforce/app/modules/profile/ProfileFirebaseRepository.kt
+++ app/src/main/java/com/gigforce/app/modules/profile/ProfileFirebaseRepository.kt
@@ -18,66 +18,35 @@ class ProfileFirebaseRepository {
     fun setProfileEducation(education: ArrayList<Education>) {
         for(ed in education) {
             firebaseDB.collection("user_profiles")
-                .document(uid).update("Education", FieldValue.arrayUnion(
-                    mapOf<String, Any>(
-                        "institution" to ed.institution,
-                        "course" to ed.course,
-                        "degree" to ed.degree,
-                        "startYear" to ed.startYear!!,
-                        "endYear" to ed.endYear!!
-                    )
-                ))
+                .document(uid).update("Education", FieldValue.arrayUnion(ed))
         }
     }
 
     fun setProfileSkill(skills: ArrayList<Skill>) {
         for(sk in skills) {
             firebaseDB.collection("user_profiles")
-                .document(uid).update("Skill", FieldValue.arrayUnion(
-                    mapOf<String, Any>(
-                        "category" to sk.category,
-                        "nameOfSkill" to sk.nameOfSkill
-                    )
-                ))
+                .document(uid).update("Skill", FieldValue.arrayUnion(sk))
         }
     }
 
     fun setProfileAchievement(achievements: ArrayList<Achievement>) {
         for (ach in achievements) {
             firebaseDB.collection("user_profiles")
-                .document(uid).update("Achievement", FieldValue.arrayUnion(
-                    mapOf<String, Any> (
-                        "title" to ach.title,
-                        "issuingAuthority" to ach.issuingAuthority,
-                        "location" to ach.location,
-                        "year" to ach.year!!
-                    )
-                ))
+                .document(uid).update("Achievement", FieldValue.arrayUnion(ach))
         }
     }
 
     fun setProfileContact(contacts: ArrayList<Contact>) {
         for (contact in contacts) {
             firebaseDB.collection("user_profiles")
-                .document(uid).update("Contact", FieldValue.arrayUnion(
-                    mapOf<String, Any> (
-                        "phone" to contact.phone,
-                        "email" to contact.email
-                    )
-                ))
+                .document(uid).update("Contact", FieldValue.arrayUnion(contact))
         }
     }
 
     fun setProfileLanguage(languages: ArrayList<Language>) {
         for (lang in languages) {
             firebaseDB.collection("user_profiles")
-                .document(uid).update("Language", FieldValue.arrayUnion(
-                    mapOf<String, Any> (
-                        "name" to lang.name,
-                        "speakingSkill" to lang.speakingSkill,
-                        "writingSkill" to lang.writingSkill
-                    )
-                ))
+                .document(uid).update("Language", FieldValue.arrayUnion(lang))
         }
     }
 }
