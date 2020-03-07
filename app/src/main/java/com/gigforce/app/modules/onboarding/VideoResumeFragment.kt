ce1cbe4c4d7838edb7a0d5af632dddfdfd64f03c
Priyadarshan <priyadarshan.cs@gig4ce.com>
2020-03-07 20:12:01 +0530

    video resume - recorder

--- app/src/main/java/com/gigforce/app/modules/onboarding/VideoResumeFragment.kt
+++ app/src/main/java/com/gigforce/app/modules/onboarding/VideoResumeFragment.kt
@@ -1,20 +1,29 @@
 package com.gigforce.app.modules.onboarding
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.*
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.Toast
 import android.widget.VideoView
+import androidx.core.graphics.drawable.toDrawable
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.gigforce.app.R
+import com.gigforce.app.R.*
 import com.gigforce.app.modules.onboarding.utils.DepthPageTransformer
 import com.gigforce.app.utils.GlideApp
 import com.gigforce.app.utils.dp
 import kotlinx.android.synthetic.main.fragment_video_resume.*
+import kotlinx.android.synthetic.main.fragment_video_resume.view.*
+import java.io.File
 
 
 class VideoResumeFragment:Fragment() {
@@ -38,13 +47,69 @@ class VideoResumeFragment:Fragment() {
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
             //window.statusBarColor() to default ie transparent
             // Inflate the layout for this fragment
-            return inflater.inflate(R.layout.fragment_video_resume, container, false)
+            return inflater.inflate(layout.fragment_video_resume, container, false)
         }
 
         override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
             super.onViewCreated(view, savedInstanceState)
+
+//            if(viewpager.currentItem <=2 )
+//            {
+//                Toast.makeText(context, "counter:>>>>>>>>>>>>>> "+viewpager.currentItem.toString(), Toast.LENGTH_SHORT).show()
+//                button_video.visibility = View.INVISIBLE
+//            }
+            if(button_video?.visibility==View.VISIBLE){
+                button_video?.setOnClickListener {
+                    dispatchTakeVideoIntent()
+                }
+            }
+/*
+            fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent) {
+                if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
+                    val videoUri: Uri? = intent.data
+                    var videoView = view.findViewById(R.id.videoView) as VideoView
+                    videoView.setVideoURI(videoUri)
+
+                    var p = videoUri.toString()//getRealPathFromURI(videoUri)
+                    val from = File(p)
+                    val onlyPath: String = from.getParentFile().getAbsolutePath()
+                    val newPath = R.raw.hello
+                    //    onlyPath + "hi.mp4"
+                    var to = File(newPath.toString())
+                    videoView.setVideoPath(to.absolutePath)
+                    videoView.start()
+                }
+            }
+*/
             this.setupViewPager()
+/*
+            play_button.setOnClickListener{
+                val intent = Intent(Intent.ACTION_VIEW)
+                onActivityResult(REQUEST_VIDEO_CAPTURE, RESULT_OK, intent )
+            }
+*/
+//            button_video.setOnClickListener (object : View.OnClickListener {
+//                val REQUEST_TAKE_GALLERY_VIDEO = 1;
+//                    override fun onClick(v: View?) {
+//                        val intent = Intent()
+//                        intent.type = "video/*"
+//                        intent.action = Intent.ACTION_GET_CONTENT
+//                        startActivityForResult(
+//                            Intent.createChooser(intent, "Select Video"),
+//                            REQUEST_TAKE_GALLERY_VIDEO
+//                        )
+//                    }
+//                })
         }
+
+
+    private fun dispatchTakeVideoIntent() {
+        Intent(MediaStore.ACTION_VIDEO_CAPTURE).also { takeVideoIntent ->
+            //takeVideoIntent.resolveActivity()?.also {
+                startActivityForResult(takeVideoIntent, Companion.REQUEST_VIDEO_CAPTURE)
+            //}
+        }
+    }
 
         fun setupViewPager(){
             this.viewpager.adapter = VideoResumeViewPagerAdapter(this.viewpager, object: OnVideoResumeCompleted(){
@@ -77,11 +142,15 @@ class VideoResumeFragment:Fragment() {
             image.layoutParams = params
 
             if(selected)
-                image.setImageResource(R.drawable.dottab_indicator_selected)
+                image.setImageResource(drawable.dottab_indicator_selected)
             else
-                image.setImageResource(R.drawable.dottab_indicator_default)
+                image.setImageResource(drawable.dottab_indicator_default)
             return image
         }
+
+    companion object {
+        const val REQUEST_VIDEO_CAPTURE = 1
+    }
 }
 
     class VideoResumeViewPagerAdapter(val viewpager: ViewPager2,
@@ -99,46 +168,46 @@ class VideoResumeFragment:Fragment() {
             var currentPosition:Int = -1
 
             init {
-                mainArtImageView = this.itemView.findViewById<ImageView>(R.id.iv_main_art_video)
-                //mainArtVideoView = this.itemView.findViewById<VideoView>(R.id.iv_main_art_video)
-
-                //nextButton = this.itemView.findViewById<Button>(R.id.btn_next)
-
-
-                    if(currentPosition < 2)
-                        viewpager.setCurrentItem(currentPosition+1, true)
-                    else if (currentPosition == 2) {
-                        // on Final CA Executed
-                        onVideoResumeCompleted.invoke()
-                    }
-
+                mainArtImageView = this.itemView.findViewById<ImageView>(id.iv_main_art_video)
             }
 
             fun Bind(position: Int){
-                currentPosition = position
+
+                //currentPosition = position
+
+
+                //Toast.makeText(holder.viewpager.rootView.context, "counter:>>>>>>>>>>>>>> "+viewpager.currentItem.toString(), Toast.LENGTH_SHORT).show()
+
+
+
                 if(position == 0) {
-
                     GlideApp.with(itemView)
-                        .load(R.drawable.ic_intro_slides1_mainart)
+                        .load(drawable.ic_intro_slides1_mainart)
                         .into(mainArtImageView)
-
+                    //viewpager.button_video?.visibility=View.INVISIBLE
+                    viewpager.rootView.button_video?.visibility=View.INVISIBLE;
                     //nextButton.setText("next")
 
                 }else if(position == 1){
                     GlideApp.with(itemView)
-                        .load(R.drawable.ic_intro_slides2_mainart)
+                        .load(drawable.ic_intro_slides2_mainart)
                         .into(mainArtImageView)
-
-
                     //nextButton.setText("next")
-
+                    Toast.makeText(viewpager.rootView.context, "counter:>>asdfas>>>>>> "+position.toString(), Toast.LENGTH_SHORT).show()
+                    //viewpager.button_video?.visibility=View.INVISIBLE
+                    viewpager.rootView.button_video?.visibility=View.INVISIBLE;
                 }else if(position == 2) {
                     GlideApp.with(itemView)
-                        .load(R.drawable.ic_intro_slides3_mainart)
+                        .load(drawable.ic_intro_slides3_mainart)
                         .into(mainArtImageView)
+                    Toast.makeText(viewpager.rootView.context, "counter:>>asdfas>>>>>> "+position.toString(), Toast.LENGTH_SHORT).show()
+                    viewpager.rootView.button_video?.visibility=View.VISIBLE;
+
+//                    mainArtImageView.setOnClickListener {
+//
+//                    }
                         //.load(R.raw.hello)
                         //.into(mainArtVideoView)
-
                     //titleTextView.setText(R.string.intro_slide3_title)
                     //subTitleTextView.setText(R.string.intro_slide3_subtitle)
                     //nextButton.setText("Get Started")
@@ -146,9 +215,10 @@ class VideoResumeFragment:Fragment() {
             }
         }
 
+
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
             val view: View = LayoutInflater.from(parent.context)
-                .inflate(R.layout.layout_video_resume_slide, parent, false)
+                .inflate(layout.layout_video_resume_slide, parent, false)
             return ViewHolder(view, viewpager, onVideoResumeCompleted)
         }
 
