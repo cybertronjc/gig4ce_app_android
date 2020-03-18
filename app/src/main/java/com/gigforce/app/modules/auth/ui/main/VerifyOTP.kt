15aeb42eb4f8196a1adb8b95b6583f76812fed8d
Priyadarshan <priyadarshan.cs@gig4ce.com>
2020-03-18 17:59:30 +0530

    login, verify otp fixes

--- app/src/main/java/com/gigforce/app/modules/auth/ui/main/VerifyOTP.kt
+++ app/src/main/java/com/gigforce/app/modules/auth/ui/main/VerifyOTP.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.gigforce.app.R
@@ -33,24 +34,19 @@ class VerifyOTP: Fragment() {
             verificationId = it.getString("verificationId")!!
       //      mTimerTextView = TextView(view?.context);
             //object : CountDownTimer(30000, 1000) {
-            object : CountDownTimer(5000, 1000) {
-                override fun onTick(millisUntilFinished: Long) {
-                    layout.otptimertv.text = (millisUntilFinished / 1000).toString() + " s"
-                }
-                override fun onFinish() {
-                    layout.otptimertv.text = "resend"
-                    if(otpresentcounter>=2){
-                        layout.otptimertv.text = "try later!"
-                        Toast.makeText(layout.context, "Too many invalid attempts, Try again later!", Toast.LENGTH_SHORT).show()
-                    }
-                    layout.otptimertv.setOnClickListener{
-                        otpresentcounter++;
-                        Toast.makeText(layout.context, "OTP resent", Toast.LENGTH_SHORT).show()
-                        viewModel.phoneNo?.let { it1 -> viewModel.sendVerificationCode(it1) }
-                    }
-                }
-            }.start()
+            counterStart();
         }
+    }
+
+    private fun counterStart(){
+        object : CountDownTimer(5000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                layout.otptimertv.text = (millisUntilFinished / 1000).toString() + " s"
+            }
+            override fun onFinish() {
+                layout.otptimertv.text = "Resend"
+            }
+        }.start()
     }
 
     override fun onCreateView(
@@ -58,9 +54,13 @@ class VerifyOTP: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewModel = ViewModelProviders.of(this.activity!!).get(LoginViewModel::class.java)
+        viewModel = ViewModelProviders.of(this).get(LoginViewModel::class.java)
+        //                                                                  viewModel = this.parentFragment?.activity?.let { ViewModelProviders.of(it).get(LoginViewModel::class.java) }!!
+        //viewModel = ViewModelProviders.of(this.activity!!).get(LoginViewModel::class.java)
         viewModel.verificationId = verificationId.toString()
         layout = inflater.inflate(R.layout.otp_verification, container, false)
+        //TODO
+        layout.textView29.text = "We have sent the OTP to your "+viewModel?.phoneNo?.toString()+"\nPlease enter the OTP";
         return layout
     }
 
@@ -74,6 +74,23 @@ class VerifyOTP: Fragment() {
             }
             //findNavController().navigate(R.id.homeFragment)
         }
+        layout.otptimertv.setOnClickListener {
+            if(layout.otptimertv.text == "Resend") {
+                otpresentcounter++;
+                Toast.makeText(layout.context, "OTP resent", Toast.LENGTH_SHORT).show()
+                viewModel.phoneNo?.let { it1 -> viewModel.sendVerificationCode(it1) }
+                counterStart();
+            }
+            else{
+                Toast.makeText(layout.context, "Click on Reenter mobile number as it could be wrong!", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        if(otpresentcounter>=2){
+            layout.otptimertv.text = "try later!"
+            Toast.makeText(layout.context, "Too many invalid attempts, Try again later!", Toast.LENGTH_SHORT).show()
+        }
+        
         layout.reenter_mobile.setOnClickListener { findNavController().navigate(R.id.mobileInput)}
     }
 }
