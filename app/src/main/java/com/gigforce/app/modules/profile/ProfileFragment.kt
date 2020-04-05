f943518ab4c1023c64e4a53e01cf53bb201c3a89
Priyadarshan <priyadarshan.cs@gig4ce.com>
2020-04-05 15:18:29 +0530

    photocrop and manifest file debugged - detect face and storage dir added in EXTRAS

--- app/src/main/java/com/gigforce/app/modules/profile/ProfileFragment.kt
+++ app/src/main/java/com/gigforce/app/modules/profile/ProfileFragment.kt
@@ -1,6 +1,5 @@
 package com.gigforce.app.modules.profile
 
-import android.app.Activity
 import android.content.Intent
 import android.graphics.Color
 import android.net.Uri
@@ -15,21 +14,15 @@ import android.widget.ImageView
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
-import com.bumptech.glide.Glide
 import com.gigforce.app.R
 import com.gigforce.app.modules.photocrop.*
+import com.gigforce.app.utils.GlideApp
 import com.google.android.material.chip.Chip
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.StorageMetadata
 import com.google.firebase.storage.StorageReference
-import kotlinx.android.synthetic.main.fragment_profile.*
 import kotlinx.android.synthetic.main.fragment_profile_education_expanded.view.*
 import kotlinx.android.synthetic.main.fragment_profile_main_expanded.view.*
-import kotlinx.android.synthetic.main.profile_card_background.view.*
-import kotlinx.android.synthetic.main.profile_main_card_background.view.card_content
-import kotlinx.android.synthetic.main.profile_main_card_background.view.card_title
-import kotlinx.android.synthetic.main.profile_main_card_background.view.*
-import kotlinx.android.synthetic.main.profile_nav_bar.view.*
 import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
@@ -44,7 +37,9 @@ class ProfileFragment : Fragment() {
     private lateinit var storage: FirebaseStorage
     private lateinit var layout: View
     private lateinit var profileAvatar: ImageView
+    private lateinit var profileAvatarName: String
     private var PHOTO_CROP: Int = 45
+    private var PROFILE_PICTURE_FOLDER: String = "profile_pics"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -64,20 +59,13 @@ class ProfileFragment : Fragment() {
 
         viewModel = ViewModelProviders.of(this).get(ProfileViewModel::class.java)
 
-        // add onClick to profile picture
-        profileAvatar = layout.findViewById(R.id.profile_avatar)
-        profileAvatar.setOnClickListener{
-            val photoCropIntent = Intent(context, PhotoCrop::class.java)
-            startActivityForResult(photoCropIntent,PHOTO_CROP)
-
-        }
-
         // load user data
         viewModel.userProfileData.observe(this, Observer { profile ->
             layout.gigger_rating.text = profile.rating!!.getTotal().toString()
             layout.task_done.text = profile.tasksDone.toString()
             layout.connection_count.text = profile.connections.toString()
             layout.main_expanded_user_name.text = profile.name
+            layout.user_about_me.text = profile.aboutMe
 
             Log.d("ProfileFragment", profile.isVerified.toString())
             if (profile.isVerified) {
@@ -101,11 +89,7 @@ class ProfileFragment : Fragment() {
                 educationString += format.format(education.startYear!!) + " - " + format.format(education.endYear!!) + "\n\n"
             }
             Log.d("ProfileFragment", educationString)
-            layout.main_education_card.card_title.text = "Education"
-            layout.main_education_card.card_content.text = educationString
-            layout.main_education_card.card_view_more.setOnClickListener {
-                findNavController().navigate(R.id.educationExpandedFragment)
-            }
+            layout.education_content.text = educationString
 
             var experienceString = ""
             for (exp in profile.Experience!!) {
@@ -114,22 +98,45 @@ class ProfileFragment : Fragment() {
                 experienceString += exp.location + "\n"
                 experienceString += format.format(exp.startDate!!) + "-" + format.format(exp.endDate!!) + "\n\n"
             }
-            layout.main_experience_card.card_title.text = "Experience"
-            layout.main_experience_card.card_content.text = experienceString
-            layout.main_experience_card.card_view_more.setOnClickListener {
-                findNavController().navigate(R.id.experienceExpandedFragment)
-            }
+            layout.experience_content.text = experienceString
 
-            layout.main_about_card.card_title.text = "About me"
-            layout.main_about_card.card_content.text = profile.bio.toString()
-            layout.main_about_card.card_view_more.setOnClickListener {
-                findNavController().navigate(R.id.aboutExpandedFragment)
-            }
+            layout.about_card_content.text = profile.bio.toString()
             Log.d("ProfileFragment", profile.rating.toString())
+
+            profileAvatarName = profile.profileAvatarName
+            Log.e("PROFILE AVATAR", profileAvatarName)
+            if (profileAvatarName != null)
+                loadImage(profileAvatarName)
         })
+
+        /*
+        Clicking on profile picture opens Photo Crop Activity
+         */
+        layout.profile_avatar.setOnClickListener {
+            val photoCropIntent = Intent(context, PhotoCrop::class.java)
+            photoCropIntent.putExtra("fbDir", "/profile_pics/")
+            photoCropIntent.putExtra("detectFace",1)
+            photoCropIntent.putExtra("folder", PROFILE_PICTURE_FOLDER)
+            photoCropIntent.putExtra("file", profileAvatarName)
+            startActivityForResult(photoCropIntent, PHOTO_CROP)
+        }
 
         layout.add_tags_button.setOnClickListener{
             this.findNavController().navigate(R.id.addTagBottomSheet)
+        }
+
+        layout.about_card_view_more_button.setOnClickListener{
+            this.findNavController().navigate(R.id.aboutExpandedFragment)
+        }
+
+        layout.education_view_more.setOnClickListener {
+            Log.d("CLICK_STATUS", "CLICK HEARD")
+            Toast.makeText(this.context, "View More Clicked", Toast.LENGTH_LONG).show()
+            this.findNavController().navigate(R.id.educationExpandedFragment)
+        }
+
+        layout.experience_card_view_more.setOnClickListener {
+            this.findNavController().navigate(R.id.experienceExpandedFragment)
         }
 
         // back page navigation
@@ -139,25 +146,9 @@ class ProfileFragment : Fragment() {
     }
 
     private fun loadImage(Path: String) {
-        val profilePicRef: StorageReference = storage.reference.child("profile_pics").child(Path)
-        Glide.with(this.context!!)
+        var profilePicRef: StorageReference = storage.reference.child("profile_pics").child(Path)
+        GlideApp.with(this.context!!)
             .load(profilePicRef)
             .into(layout.profile_avatar)
-    }
-
-    override fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        data: Intent?
-    ): Unit {
-
-        super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode == PHOTO_CROP && resultCode == Activity.RESULT_OK){
-            var imageName: String? = data?.getStringExtra("filename")
-            Log.v("PROFILE_FRAG_OAR","filename is:"+imageName)
-            if(null!=imageName){
-                loadImage(imageName)
-            }
-        }
     }
 }
