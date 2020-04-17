82f3e4b63ebc079bd6f14b0fe16c07647bf543e4
Priyadarshan <priyadarshan.cs@gig4ce.com>
2020-04-17 22:12:18 +0530

    merged with Amit changes

--- app/src/main/java/com/gigforce/app/modules/homescreen/HomeScreenAdapter.kt
+++ app/src/main/java/com/gigforce/app/modules/homescreen/HomeScreenAdapter.kt
@@ -3,63 +3,29 @@ import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.view.LayoutInflater
-import android.widget.ArrayAdapter
-import android.widget.ImageView
-import android.widget.TextView
+import android.widget.BaseAdapter
 import com.gigforce.app.R
+import kotlinx.android.synthetic.main.item_gridhomescreen.view.*
 
-internal class HomeScreenAdapter internal constructor(context: Context, private val resource: Int, private val itemList: Array<String>?) : ArrayAdapter<HomeScreenAdapter.ItemHolder>(context, resource) {
-
+class HomeScreenAdapter(private val context: Context, private val resource: Int, private val itemList: ArrayList<HSGridItemDataModel>?) :
+    BaseAdapter() {
     override fun getCount(): Int {
         return if (this.itemList != null) this.itemList.size else 0
     }
 
-    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        var convertView = convertView
-
-        val holder: ItemHolder
-        if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(resource, null)
-            holder = ItemHolder()
-            holder.name = convertView!!.findViewById(R.id.textView)
-            holder.icon = convertView.findViewById(R.id.icon)
-            convertView.tag = holder
-        } else {
-            holder = convertView.tag as ItemHolder
-        }
-
-        holder.name!!.text = this.itemList!![position]
-        when (position) {
-            0 -> {holder.icon!!.setImageResource(R.drawable.ic_homescreen_profile)
-                holder.name!!.text = "Profile"}
-            1 -> {
-                holder.icon!!.setImageResource(R.drawable.ic_homescreen_learn)
-                holder.name!!.text = "Learning"}
-            2 -> {holder.icon!!.setImageResource(R.drawable.ic_homescreen_payment)
-                holder.name!!.text = "Payment"}
-            3 -> {holder.icon!!.setImageResource(R.drawable.ic_homescreen_explore)
-                holder.name!!.text = "Search"}
-            4 -> {holder.icon!!.setImageResource(R.drawable.ic_homescreen_chat)
-                holder.name!!.text = "Chat"}
-            5 -> {holder.icon!!.setImageResource(R.drawable.ic_homescreen_pref)
-                holder.name!!.text = "Support"}
-            6 -> {holder.icon!!.setImageResource(R.drawable.ic_homescreen_control)
-                holder.name!!.text = "Preferences"}
-            7 -> {holder.icon!!.setImageResource(R.drawable.gig4ce_logo)
-                holder.name!!.text = "Video Resume"}
-            8 -> {holder.icon!!.setImageResource(R.drawable.ic_homescreen_explore)
-                holder.name!!.text = "New HomeScreen"}
-            else -> { // Note the block
-                holder.icon!!.setImageResource(R.drawable.gig4ce_logo)
-                holder.name!!.text = "More"
-            }
-        }
-
-        return convertView
+    override fun getItem(position: Int): Any {
+        return itemList!![position]
     }
 
-    internal class ItemHolder {
-        var name: TextView? = null
-        var icon: ImageView? = null
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var inflator = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        var foodView = inflator.inflate(R.layout.item_gridhomescreen, null)
+        foodView.textView.text = this.itemList!![position].iconName
+        foodView.icon.setImageResource(this.itemList!![position].icon)
+        return foodView
     }
 }
--- app/src/main/java/com/gigforce/app/modules/homescreen/HomeScreenIcons.kt
+++ app/src/main/java/com/gigforce/app/modules/homescreen/HomeScreenIcons.kt
@@ -1,19 +1,18 @@
 package com.gigforce.app.modules.homescreen
 
+import android.app.Dialog
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.*
 import android.widget.AdapterView
-import android.widget.GridView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.gigforce.app.R
-import com.gigforce.app.modules.auth.utils.SignoutTask
-import com.gigforce.app.modules.profile.models.ProfileData
-import com.gigforce.app.modules.verification.VeriFirebaseRepository
+import com.gigforce.app.core.base.BaseFragment
 import com.gigforce.app.utils.setDarkStatusBarTheme
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.*
@@ -22,66 +21,120 @@ import kotlinx.android.synthetic.main.layout_home_screen.*
 import kotlinx.android.synthetic.main.layout_home_screen.view.*
 
 
-class HomeScreenIcons : Fragment() {
+class HomeScreenIcons : BaseFragment() {
 
     //todo
     private lateinit var storage: FirebaseStorage
     var firebaseDB = FirebaseFirestore.getInstance()
     var uid = FirebaseAuth.getInstance().currentUser?.uid!! //ynLyPDjsBrYgiFT3OWX8Tn8OLjI2 or GigerId1
-    private lateinit var layout: View
 
     private lateinit var docref: DocumentReference;
-    private val itemList: Array<String>
-        get() = arrayOf("Profile", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7", "Item 8", "Item 9", "Item 10", "Item 11", "Item 12")
-
-
-    var counter = 0
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-
-            /*
-                          check the kyc flag and video resume flag and accordingly set the card views visibility
-                           */
-        //layout.cardviewkyc.visibility
-            //layout.text_kyc.visibility=View.VISIBLE
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-
-        // get instance of the firebase storage
         storage = FirebaseStorage.getInstance()
-        layout = inflater.inflate(R.layout.layout_home_screen, container, false)
-
-        return layout
+        return inflateView(R.layout.layout_home_screen, inflater, container)
     }
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         this.setDarkStatusBarTheme(false)
-        val gridview = layout.findViewById<GridView>(R.id.gridview)
-
-        val adapter = this.context?.let { HomeScreenAdapter(it, R.layout.item_gridhomescreen, itemList) }
-        gridview.adapter = adapter
-
+        initialize()
+        listener()
+        dbCall()
         //topbar.setOnClickListener { findNavController().navigate(R.id.profileFragment) }
-        cardviewkyc.text_kyc.setOnClickListener {
+    }
 
-            /** Algo:
-                    Check the KYC flag of Giger Profile from Profiles collection
-                    check which flag is true or false
-                    if address is false - go to verification
-                    if address is true, aadhaar is false and  all of (dl,voterid,passport) are false - go to aadhaarUpload
-                    if address is true, aadhaar is false and any of (dl,voterid,passport) is true - go to bankUpload
-                    if address and aadhaar are true, bank is false - go to bankUpload
-                    if address and aadhaar are true, bank is true - go to UploadPan
-             */
+    private fun dbCall() {
+        uid = "GigerId1";
+        firebaseDB.collection("Verification").document(uid).addSnapshotListener(EventListener<DocumentSnapshot> {
+                value, e ->
+            if (e != null) {
+                Log.w("HomeScreenIcons", "Listen failed", e)
+                return@EventListener
+            }
+
+            Log.d("HomeScreenIcons", value.toString())
+            value?.data?.entries?.forEach { (k,v)->if(k == "bio_kyc_verified"){
+            }}
+        })
+    }
+
+    private fun listener() {
+        cardviewkyc.text_kyc.setOnClickListener {
+            navigate(R.id.verification)
+            //findNavController().navigate(R.id.uploadDropDown)
+        }
+        cardviewvideoresume.text_kyc_video.setOnClickListener {
+            Toast.makeText(context, "TODO CTA: jump to video resume upload page", Toast.LENGTH_SHORT).show()
+        }
+        button_signout.setOnClickListener {
+            showConfirmationDialog()
+
+        }
+    }
+
+    private fun showConfirmationDialog() {
+        val dialog = activity?.let { Dialog(it) }
+        dialog?.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog?.setCancelable(false)
+        dialog?.setContentView(R.layout.signout_custom_alert)
+        val titleDialog = dialog?.findViewById(R.id.title) as TextView
+        titleDialog.text = "Do you really want to sign out?"
+        val yesBtn = dialog?.findViewById(R.id.yes) as TextView
+        val noBtn = dialog?.findViewById(R.id.cancel) as TextView
+        yesBtn.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            dialog?.dismiss()
+        }
+        noBtn.setOnClickListener { dialog .dismiss() }
+        dialog?.show()
+    }
+
+
+    private fun initialize() {
+        var gridItems : ArrayList<HSGridItemDataModel> = getGridItems()
+        val adapter = this.context?.let { HomeScreenAdapter(it, R.layout.item_gridhomescreen, gridItems) }
+        gridview.adapter = adapter
+        gridview.onItemClickListener = AdapterView.OnItemClickListener { parent, v, position, id ->
+            // Write code to perform action when item is clicked.
+            if(gridItems.get(position).navigateToFragment!=0)
+            navigate(gridItems.get(position).navigateToFragment)
+            else
+                showToast("TODO CTA ")
+        }
+
+    }
+
+    private fun getGridItems(): ArrayList<HSGridItemDataModel> {
+        var gridItems : ArrayList<HSGridItemDataModel> = ArrayList<HSGridItemDataModel>()
+        gridItems.add(HSGridItemDataModel("Profile",R.drawable.ic_homescreen_profile,R.id.profileFragment))
+        gridItems.add(HSGridItemDataModel("Learning",R.drawable.ic_homescreen_learn,0))
+        gridItems.add(HSGridItemDataModel("Payment",R.drawable.ic_homescreen_payment,0))
+        gridItems.add(HSGridItemDataModel("Search",R.drawable.ic_homescreen_explore,0))
+        gridItems.add(HSGridItemDataModel("Chat",R.drawable.ic_homescreen_chat,0))
+        gridItems.add(HSGridItemDataModel("Support",R.drawable.ic_homescreen_pref,0))
+        gridItems.add(HSGridItemDataModel("Preferences",R.drawable.ic_homescreen_control,0))
+        gridItems.add(HSGridItemDataModel("Video Resume",R.drawable.gig4ce_logo,R.id.videoResumeFragment))
+	gridItems.add(HSGridItemDataModel("New HomeScreen",R.drawable.gig4ce_logo,R.id.homeScreenNew))
+        gridItems.add(HSGridItemDataModel("More",R.drawable.gig4ce_logo,0))
+        gridItems.add(HSGridItemDataModel("Prefrences",R.drawable.ic_homescreen_pref,R.id.settingFragment))
+        return gridItems
+    }
+}
+
+
+/** Algo:
+Check the KYC flag of Giger Profile from Profiles collection
+check which flag is true or false
+if address is false - go to verification
+if address is true, aadhaar is false and  all of (dl,voterid,passport) are false - go to aadhaarUpload
+if address is true, aadhaar is false and any of (dl,voterid,passport) is true - go to bankUpload
+if address and aadhaar are true, bank is false - go to bankUpload
+if address and aadhaar are true, bank is true - go to UploadPan
+ */
 //            docref = firebaseDB.collection("Verification").document(uid);
 //            docref.get()
 //                .addOnSuccessListener { document ->
@@ -98,65 +151,14 @@ class HomeScreenIcons : Fragment() {
 //                .addOnFailureListener { exception ->
 //                    Log.d("TAG", "get failed with ", exception)
 //                }
-            findNavController().navigate(R.id.verification)
-            //findNavController().navigate(R.id.uploadDropDown)
-        //    Toast.makeText(context, "TODO CTA: jump to kyc docs upload page", Toast.LENGTH_SHORT).show()
-        }
-        cardviewvideoresume.text_kyc_video.setOnClickListener {
-            Toast.makeText(context, "TODO CTA: jump to video resume upload page", Toast.LENGTH_SHORT).show()
-        }
-
-        gridview.onItemClickListener = AdapterView.OnItemClickListener { parent, v, position, id ->
-            // Write code to perform action when item is clicked.
-
-            when (position) {
-                0 -> {findNavController().navigate(R.id.profileFragment)}
-                1 -> {
-                    Toast.makeText(context, "TODO CTA: $position", Toast.LENGTH_SHORT).show()
-                }
-                2 -> {
-                    //findNavController().navigate(R.id.profileFragment)
-                    Toast.makeText(context, "TODO CTA: $position", Toast.LENGTH_SHORT).show()
-                }
-                7 -> {
-                    findNavController().navigate(R.id.videoResumeFragment)
-                    //Toast.makeText(context, "TODO CTA: $position", Toast.LENGTH_SHORT).show()
-                }
-                8 -> {
-                    findNavController().navigate(R.id.homeScreenNew)
-                }
-                else -> {
-                    Toast.makeText(context, "TODO CTA: $position", Toast.LENGTH_SHORT).show()
-                }
-            }
-
-            if(position==0){
-
-            }
-        }
-
-        layout.button_signout.setOnClickListener { FirebaseAuth.getInstance().signOut() }
-        Log.d(">>>>>>>>>>>",uid);
-        uid = "GigerId1";
 
 
-//        firebaseDB.collection("Verification").whereEqualTo(FieldPath.documentId(),"GigerId1").whereEqualTo()
 
-        firebaseDB.collection("Verification").document(uid).addSnapshotListener(EventListener<DocumentSnapshot> {
-                value, e ->
-            if (e != null) {
-                Log.w("HomeScreenIcons", "Listen failed", e)
-                return@EventListener
-            }
-
-            Log.d("HomeScreenIcons", value.toString())
 //
 //            Toast.makeText(context,
 //                "TODO CTA:"+value?.data?.keys.toString(), Toast.LENGTH_SHORT).show()
 
-            value?.data?.entries?.forEach { (k,v)->if(k == "bio_kyc_verified"){
-//                Toast.makeText(context,"TODO CTA: $v", Toast.LENGTH_SHORT).show()
-            }}
+//        firebaseDB.collection("Verification").whereEqualTo(FieldPath.documentId(),"GigerId1").whereEqualTo()
 
 //            value?.data?.keys?.forEach { k-> if(k.equals("bio_kyc_verified")){
 //
@@ -172,8 +174,8 @@ class HomeScreenIcons : Fragment() {
 //            }}
 
 
-
-
-        })
-    }
-}
+/*
+              check the kyc flag and video resume flag and accordingly set the card views visibility
+               */
+//layout.cardviewkyc.visibility
+//layout.text_kyc.visibility=View.VISIBLE
