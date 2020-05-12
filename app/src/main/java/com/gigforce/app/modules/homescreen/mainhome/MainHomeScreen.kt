eae729db93ef0b95a83e790414d358ace16e6f51
Amit <amitdhiman13190@gmail.com>
2020-05-13 01:46:34 +0530

    homescreen bottom up sheet complete

--- app/src/main/java/com/gigforce/app/modules/homescreen/mainhome/MainHomeScreen.kt
+++ app/src/main/java/com/gigforce/app/modules/homescreen/mainhome/MainHomeScreen.kt
@@ -31,7 +31,7 @@ import com.gigforce.app.modules.homescreen.mainhome.bottomsheet.UpcomingGigModel
 import com.gigforce.app.modules.homescreen.mainhome.verticalcalendar.VerticalCalendarDataItemModel
 import com.gigforce.app.modules.preferences.PreferencesFragment
 import com.gigforce.app.modules.profile.ProfileViewModel
-//import com.gigforce.app.utils.GlideApp
+import com.gigforce.app.utils.GlideApp
 import com.google.firebase.storage.StorageReference
 import com.riningan.widget.ExtendedBottomSheetBehavior
 import com.riningan.widget.ExtendedBottomSheetBehavior.STATE_COLLAPSED
@@ -50,29 +50,12 @@ class MainHomeScreen : BaseFragment() {
     private var mExtendedBottomSheetBehavior: ExtendedBottomSheetBehavior<*>? = null
     private lateinit var viewModel: MainHomeScreenViewModel
     lateinit var viewModelProfile: ProfileViewModel
-    private val itemList: Array<String>
-        get() = arrayOf(
-            "My Gig",
-            "Item 2",
-            "Item 3",
-            "Item 4",
-            "Item 5",
-            "Item 6",
-            "Item 7",
-            "Item 8",
-            "Item 9",
-            "Item 10",
-            "Item 11",
-            "Item 12"
-        )
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflateView(R.layout.homescreen_1nsvbs, inflater, container)
     }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -89,17 +72,7 @@ class MainHomeScreen : BaseFragment() {
     private fun initializeViews() {
         initialiseMonthTV()
         initializeVerticalCalendarRV()
-        initializeBottomSheet()
     }
-
-    private fun initializeBottomSheet() {
-//        nsv.setBackground(generateBackgroundWithShadow(nsv,R.color.white,
-//            R.dimen.eight_dp,R.color.gray_color,R.dimen.five_dp, Gravity.TOP))
-        initializeUpcomingGigBottomSheet()
-//        initializeFeaturesBottomSheet()
-    }
-
-
 
     private fun listener() {
         iv2HS1.setOnClickListener(View.OnClickListener { navigate(R.id.profileFragment) })
@@ -337,124 +310,4 @@ class MainHomeScreen : BaseFragment() {
         }
     }
 
-    private fun initializeUpcomingGigBottomSheet(){
-        nsv.setBackground(generateBackgroundWithShadow(nsv,R.color.white,
-            R.dimen.eight_dp,R.color.gray_color,R.dimen.five_dp, Gravity.TOP))
-        val displayMetrics = DisplayMetrics()
-        activity?.windowManager?.getDefaultDisplay()?.getMetrics(displayMetrics)
-        val width = displayMetrics.widthPixels
-        val itemWidth = ((width / 5)*4).toInt()
-        var datalist: ArrayList<UpcomingGigModel> = ArrayList<UpcomingGigModel>()
-        datalist.add(UpcomingGigModel())
-        datalist.add(UpcomingGigModel())
-        val recyclerGenericAdapter: RecyclerGenericAdapter<UpcomingGigModel> =
-            RecyclerGenericAdapter<UpcomingGigModel>(
-                activity?.applicationContext,
-                PFRecyclerViewAdapter.OnViewHolderClick<Any?> { view, position, item -> showToast("")},
-                RecyclerGenericAdapter.ItemInterface<UpcomingGigModel?> { obj, viewHolder, position ->
-                    val lp = getView(viewHolder,R.id.card_view).layoutParams
-                    lp.height = lp.height
-                    lp.width = itemWidth
-                    getView(viewHolder,R.id.card_view).layoutParams = lp
-                })!!
-        recyclerGenericAdapter.setList(datalist)
-        recyclerGenericAdapter.setLayout(R.layout.upcoming_gig_item)
-        upcoming_gig_rv.layoutManager = LinearLayoutManager(
-            activity?.applicationContext,
-            LinearLayoutManager.HORIZONTAL,
-            false
-        )
-        upcoming_gig_rv.adapter = recyclerGenericAdapter
-
-    }
-    fun generateBackgroundWithShadow(
-        view: View, @ColorRes backgroundColor: Int,
-        @DimenRes cornerRadius: Int,
-        @ColorRes shadowColor: Int,
-        @DimenRes elevation: Int,
-        shadowGravity: Int
-    ): Drawable? {
-        val cornerRadiusValue =
-            view.context.resources.getDimension(cornerRadius)
-        val elevationValue = view.context.resources.getDimension(elevation).toInt()
-        val shadowColorValue = ContextCompat.getColor(view.context, shadowColor)
-        val backgroundColorValue = ContextCompat.getColor(view.context, backgroundColor)
-        val outerRadius = floatArrayOf(
-            cornerRadiusValue, cornerRadiusValue, cornerRadiusValue,
-            cornerRadiusValue, cornerRadiusValue, cornerRadiusValue, cornerRadiusValue,
-            cornerRadiusValue
-        )
-        val backgroundPaint = Paint()
-        backgroundPaint.setStyle(Paint.Style.FILL)
-        backgroundPaint.setShadowLayer(cornerRadiusValue, 0F, 0F, 0)
-        val shapeDrawablePadding = Rect()
-        shapeDrawablePadding.left = elevationValue
-        shapeDrawablePadding.right = elevationValue
-        val DY: Int
-        when (shadowGravity) {
-            Gravity.CENTER -> {
-                shapeDrawablePadding.top = elevationValue
-                shapeDrawablePadding.bottom = elevationValue
-                DY = 0
-            }
-            Gravity.TOP -> {
-                shapeDrawablePadding.top = elevationValue * 2
-                shapeDrawablePadding.bottom = elevationValue
-                DY = -1 * elevationValue / 3
-            }
-            Gravity.BOTTOM -> {
-                shapeDrawablePadding.top = elevationValue
-                shapeDrawablePadding.bottom = elevationValue * 2
-                DY = elevationValue / 3
-            }
-            else -> {
-                shapeDrawablePadding.top = elevationValue
-                shapeDrawablePadding.bottom = elevationValue * 2
-                DY = elevationValue / 3
-            }
-        }
-        val shapeDrawable = ShapeDrawable()
-        shapeDrawable.setPadding(shapeDrawablePadding)
-        shapeDrawable.paint.color = backgroundColorValue
-        shapeDrawable.paint
-            .setShadowLayer(cornerRadiusValue / 3, 0f, DY.toFloat(), shadowColorValue)
-        view.setLayerType(View.LAYER_TYPE_SOFTWARE, shapeDrawable.paint)
-        shapeDrawable.shape = RoundRectShape(outerRadius, null, null)
-        val drawable =
-            LayerDrawable(arrayOf<Drawable>(shapeDrawable))
-        drawable.setLayerInset(
-            0,
-            elevationValue,
-            elevationValue * 2,
-            elevationValue,
-            elevationValue * 2
-        )
-        return drawable
-    }
-
-    private fun initializeFeaturesBottomSheet() {
-        var datalist: ArrayList<FeatureModel> = ArrayList<FeatureModel>()
-        datalist.add(FeatureModel("MyGig",R.drawable.mygig))
-        datalist.add(FeatureModel("Explore",R.drawable.ic_search_calendar))
-        datalist.add(FeatureModel("Wallet",R.drawable.wallet))
-        datalist.add(FeatureModel("Profile",R.drawable.profile))
-        datalist.add(FeatureModel("Learning",R.drawable.learning))
-        datalist.add(FeatureModel("Settings",R.drawable.settings))
-        datalist.add(FeatureModel("Chat",R.drawable.chat))
-        datalist.add(FeatureModel("Chat",R.drawable.chat))
-
-        val recyclerGenericAdapter: RecyclerGenericAdapter<FeatureModel> =
-            RecyclerGenericAdapter<FeatureModel>(
-                activity?.applicationContext,
-                PFRecyclerViewAdapter.OnViewHolderClick<Any?> { view, position, item -> showToast("")},
-                RecyclerGenericAdapter.ItemInterface<FeatureModel?> { obj, viewHolder, position ->
-                        getImageView(viewHolder,R.id.feature_icon).setImageResource(obj?.icon!!)
-                        getTextView(viewHolder,R.id.feature_title).text = obj.title
-
-                })!!
-        recyclerGenericAdapter.setList(datalist)
-        recyclerGenericAdapter.setLayout(R.layout.feature_item)
-//        feature_rv.setLayoutManager(GridLayoutManager(activity, 2,GridLayoutManager.HORIZONTAL,false));
-//        feature_rv.adapter = recyclerGenericAdapter
-    }
 }
