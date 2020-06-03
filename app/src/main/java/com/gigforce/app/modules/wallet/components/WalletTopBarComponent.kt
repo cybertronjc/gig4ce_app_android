d86cc4211104c57ece7b2a1e51446755a8d3a448
1995YogeshSharma <yogesh.sharma@research.iiit.ac.in>
2020-06-03 06:42:38 +0530

    wip

--- app/src/main/java/com/gigforce/app/modules/wallet/components/WalletTopBarComponent.kt
+++ app/src/main/java/com/gigforce/app/modules/wallet/components/WalletTopBarComponent.kt
@@ -4,7 +4,12 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.bumptech.glide.request.RequestOptions
 import com.gigforce.app.R
+import com.gigforce.app.modules.preferences.PreferencesFragment.Companion.storage
+import com.gigforce.app.utils.GlideApp
+import com.google.firebase.storage.StorageReference
+import kotlinx.android.synthetic.main.top_profile_bar.view.*
 
 class WalletTopBarComponent: ConstraintLayout {
     constructor(context: Context): super(context)
@@ -12,6 +17,24 @@ class WalletTopBarComponent: ConstraintLayout {
 
     init {
         View.inflate(context, R.layout.wallet_top_bar_component, this)
+        imageName = "ysharma.jpg"
     }
+
+    private fun displayImage() {
+        if (imageName != null) {
+            val profilePicRef: StorageReference =
+                storage.reference.child("profile_pics").child(imageName!!)
+            GlideApp.with(this.context!!)
+                .load(profilePicRef)
+                .apply(RequestOptions().circleCrop())
+                .into(profile_image)
+        }
+    }
+
+    var imageName: String? = null
+       set(value) {
+           field = value
+           displayImage()
+       }
 
 }
