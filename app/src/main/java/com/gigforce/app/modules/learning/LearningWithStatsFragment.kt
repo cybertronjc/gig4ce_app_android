871bac72d2f7ab2310e13650c000ea1df698d174
Himanshu <him.khati@gmail.com>
2020-06-16 22:59:41 +0530

    LearningStat Fragment Made

---
+++ app/src/main/java/com/gigforce/app/modules/learning/LearningWithStatsFragment.kt
@@ -0,0 +1,28 @@
+package com.gigforce.app.modules.learning
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gigforce.app.R
+import com.gigforce.app.core.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_main_learning.*
+
+class LearningWithStatsFragment : BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = inflateView(R.layout.fragment_learning_stats, inflater, container)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        learningBackButton.setOnClickListener {
+            activity?.onBackPressed()
+        }
+
+
+    }
+
+}
