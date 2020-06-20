a0ada8e33f080277d12fd9d8f27cc7ce55b870f2
Himanshu <him.khati@gmail.com>
2020-06-20 18:15:16 +0530

    Video Play back Issues Solved

--- app/src/main/java/com/gigforce/app/modules/learning/learningVideo/PlayVideoDialogFragment.kt
+++ app/src/main/java/com/gigforce/app/modules/learning/learningVideo/PlayVideoDialogFragment.kt
@@ -1,25 +1,122 @@
 package com.gigforce.app.modules.learning.learningVideo
 
 
+import android.content.pm.ActivityInfo
+import android.content.res.Configuration
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.gigforce.app.R
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.MediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import kotlinx.android.synthetic.main.fragment_play_video.*
 
 
 class PlayVideoDialogFragment : DialogFragment() {
+
+    private var playWhenReady = true
+    private var currentWindow = 0
+    private var playbackPosition: Long = 0
+
+    private var player: SimpleExoPlayer? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ) = inflater.inflate(R.layout.fragment_play_video, container, false)
+    ) : View {
 
+        savedInstanceState?.let {
+            playWhenReady = it.getBoolean("key_play_when_ready")
+            currentWindow = it.getInt("key_current_video")
+            playbackPosition = it.getLong("key_play_back_position")
+        }
+       return inflater.inflate(R.layout.fragment_play_video, container, false)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putBoolean("key_play_when_ready", playWhenReady)
+        outState.putInt("key_current_video", currentWindow)
+        outState.putLong("key_play_back_position", playbackPosition)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setStyle(DialogFragment.STYLE_NORMAL,
-            android.R.style.Theme_Black_NoTitleBar_Fullscreen);
+        setStyle(
+            STYLE_NORMAL,
+            android.R.style.Theme_Black_NoTitleBar_Fullscreen
+        )
+    }
+
+    private fun initializePlayer() {
+        player = SimpleExoPlayer.Builder(requireContext()).build()
+        playerView.player = player
+        val uri =
+            Uri.parse("https://storage.googleapis.com/exoplayer-test-media-0/BigBuckBunny_320x180.mp4")
+        val mediaSource = buildMediaSource(uri)
+
+        player?.playWhenReady = playWhenReady
+        player?.seekTo(currentWindow, playbackPosition)
+        player?.prepare(mediaSource, false, false)
+    }
+
+    private fun releasePlayer() {
+        if (player != null) {
+            playbackPosition = player!!.currentPosition
+            currentWindow = player!!.currentWindowIndex
+            playWhenReady = player!!.playWhenReady
+            player!!.release()
+            player = null
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        if (Build.VERSION.SDK_INT <= 23)
+            releasePlayer()
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        if (Build.VERSION.SDK_INT > 23)
+            releasePlayer()
+    }
+
+    private fun buildMediaSource(uri: Uri): MediaSource {
+        val dataSourceFactory = DefaultDataSourceFactory(requireContext(), "gig4ce-agent")
+        return ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(uri)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (Build.VERSION.SDK_INT > 23)
+            initializePlayer()
+
+        fullScreenIV.setOnClickListener {
+
+            when (resources.configuration.orientation) {
+                Configuration.ORIENTATION_PORTRAIT -> activity?.requestedOrientation =
+                    ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                Configuration.ORIENTATION_LANDSCAPE -> activity?.requestedOrientation =
+                    ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (Build.VERSION.SDK_INT <= 23)
+            initializePlayer()
     }
 
 
