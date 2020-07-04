fcadcf85322e8b3fa9e3de99d4c123532f23fb61
1995YogeshSharma <yogesh.sharma@research.iiit.ac.in>
2020-07-04 10:30:41 +0530

    update last updated text

--- app/src/main/java/com/gigforce/app/modules/wallet/components/PaymentSummaryComponent.kt
+++ app/src/main/java/com/gigforce/app/modules/wallet/components/PaymentSummaryComponent.kt
@@ -1,18 +1,27 @@
 package com.gigforce.app.modules.wallet.components
 
 import android.content.Context
+import android.os.Build
 import android.util.AttributeSet
 import android.view.View
+import androidx.annotation.RequiresApi
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.gigforce.app.R
 import kotlinx.android.synthetic.main.payment_summary_component.view.*
+import java.time.LocalDateTime
 
+@RequiresApi(Build.VERSION_CODES.O)
 class PaymentSummaryComponent: ConstraintLayout {
     constructor(context: Context): super(context)
     constructor(context: Context, attrs: AttributeSet): super(context, attrs)
 
     init {
         View.inflate(context, R.layout.payment_summary_component, this)
+
+        monthly_earning_card_text.text = LocalDateTime.now().month.toString().toLowerCase()
+
+        payment_dispute_card_text.text = String.format(
+            "Last updated, %02d %s %04d", LocalDateTime.now().dayOfMonth, LocalDateTime.now().month.toString().toLowerCase(), LocalDateTime.now().year )
     }
 
     var monthlyEarning: Int = 0
