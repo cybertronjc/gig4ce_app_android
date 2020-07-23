2725d48f9c062bb9e52898c294afe033756be82a
1995YogeshSharma <yogesh.sharma@research.iiit.ac.in>
2020-07-23 08:33:43 +0530

    chat view model for fetching / updating chat header and msgs

---
+++ app/src/main/java/com/gigforce/app/modules/chatmodule/models/Message.kt
@@ -0,0 +1,19 @@
+package com.gigforce.app.modules.chatmodule.models
+
+import com.google.firebase.Timestamp
+import com.google.firebase.firestore.DocumentId
+
+class Message(
+    @DocumentId
+    var id: String = "",
+    var headerId: String = "",
+    var forUserId: String = "",
+    var otherUserId: String = "",
+    var flowType: String = "",
+    var otherMsgId: String = "",
+    var timestamp: Timestamp? = null,
+    var status: Int = 0,
+    var type: String = "",
+    var content: String = ""
+) {
+}
