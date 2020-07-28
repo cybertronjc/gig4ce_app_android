8e33eb37c87a6a8d0276887b4f6bbf0c7b1eb840
Rohit Sharrrrrrrrrrma <rohit.sharma@gig4ce.com>
2020-07-28 07:56:38 +0530

    Logo Bug Fixed

--- app/src/main/java/com/gigforce/app/modules/calendarscreen/maincalendarscreen/CalendarHomeScreen.kt
+++ app/src/main/java/com/gigforce/app/modules/calendarscreen/maincalendarscreen/CalendarHomeScreen.kt
@@ -20,7 +20,6 @@ import com.gigforce.app.R
 import com.gigforce.app.core.base.BaseFragment
 import com.gigforce.app.core.base.components.CalendarView
 import com.gigforce.app.core.base.dialog.ConfirmationDialogOnClickListener
-import com.gigforce.app.core.base.dialog.OptionSelected
 import com.gigforce.app.core.genericadapter.PFRecyclerViewAdapter
 import com.gigforce.app.core.genericadapter.RecyclerGenericAdapter
 import com.gigforce.app.core.gone
@@ -615,12 +614,12 @@ class CalendarHomeScreen : BaseFragment(),
                                 object : ConfirmationDialogOnClickListener {
                                     override fun clickedOnYes(dialog: Dialog?) {
                                         showToast("Screen is pending to show List of gigs on this day.")
-                                        makeChangesToCalendarItem(position,true)
+                                        makeChangesToCalendarItem(position, true)
                                         dialog?.dismiss()
                                     }
 
                                     override fun clickedOnNo(dialog: Dialog?) {
-                                        makeChangesToCalendarItem(position,true)
+                                        makeChangesToCalendarItem(position, true)
                                         dialog?.dismiss()
                                     }
 
@@ -636,13 +635,14 @@ class CalendarHomeScreen : BaseFragment(),
                     })
             }
         } else if (temporaryData.isUnavailable) {
-            makeChangesToCalendarItem(position,false)
+            makeChangesToCalendarItem(position, false)
         } else {
-            makeChangesToCalendarItem(position,true)
+            makeChangesToCalendarItem(position, true)
             showSnackbar(position)
         }
     }
-    fun makeChangesToCalendarItem(position: Int,status:Boolean){
+
+    fun makeChangesToCalendarItem(position: Int, status: Boolean) {
         temporaryData.isUnavailable = status
         var data = UnavailableDataModel(temporaryData.getDateObj())
         data.dayUnavailable = status
@@ -652,6 +652,7 @@ class CalendarHomeScreen : BaseFragment(),
         )
         recyclerGenericAdapter.notifyItemChanged(position)
     }
+
     class OnSnackBarUndoClickListener(
         var position: Int,
         var recyclerGenericAdapter: RecyclerGenericAdapter<VerticalCalendarDataItemModel>,
