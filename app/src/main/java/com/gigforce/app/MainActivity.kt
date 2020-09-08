94a7ba4d7af5077b400a77853dc44ff6f9bbc79c
Himanshu <him.khati@gmail.com>
2020-09-08 13:24:33 +0530

    Build Updates

--- app/src/main/java/com/gigforce/app/MainActivity.kt
+++ app/src/main/java/com/gigforce/app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
---
+++ app/src/main/java/com/gigforce/app/notification/NotificationConstants.kt
@@ -0,0 +1,14 @@
+package com.gigforce.app.notification
+
+object NotificationConstants {
+
+    object CLICK_ACTIONS {
+
+        const val OPEN_GIG_ATTENDANCE_PAGE = "com.gigforce.app.gig.open_gig_attendance_page"
+    }
+
+    object PAYLOADS{
+        const val GIG_ID = "gig_id"
+    }
+
+}
