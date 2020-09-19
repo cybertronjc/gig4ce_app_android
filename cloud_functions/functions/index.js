a975c24fc02043eb6b1553efbfd8c1a41a3fdb8b
Himanshu <him.khati@gmail.com>
2020-09-19 11:25:01 +0530

    removed firebase cloud function scripts

--- cloud_functions/functions/index.js
+++
@@ -1,6 +0,0 @@
-/*jshint esversion: 6 */
-
-const documentNotSubmitted = require('./doc_not_submitted');
-
-exports.sendDocumentsNotUploadedNotification = documentNotSubmitted.sendDocumentsNotUploadedNotification;
-
