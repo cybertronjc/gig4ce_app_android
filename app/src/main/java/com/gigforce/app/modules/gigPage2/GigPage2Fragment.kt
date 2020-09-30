3eb34c87dc1a17a09f025f5aecc9e6cdfc5a852f
Himanshu <him.khati@gmail.com>
2020-09-30 13:14:55 +0530

    Update 1

---
+++ app/src/main/java/com/gigforce/app/modules/gigPage2/GigPage2Fragment.kt
@@ -0,0 +1,22 @@
+package com.gigforce.app.modules.gigPage2
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gigforce.app.R
+import com.gigforce.app.core.base.BaseFragment
+
+class GigPage2Fragment  : BaseFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) = inflateView(R.layout.fragment_gig_page_2, inflater, container)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
