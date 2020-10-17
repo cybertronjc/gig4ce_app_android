04cc16f7aa2703c4e40b65cc9c560147b417f2d8
Himanshu <him.khati@gmail.com>
2020-10-17 11:13:01 +0530

    Added Import

--- app/src/main/java/com/gigforce/app/notification/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/gigforce/app/notification/MyFirebaseMessagingService.kt
@@ -10,6 +10,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import mobile.jobKahaHai.notification.NotificationHelper
 import java.util.*
 import kotlin.random.Random
 
