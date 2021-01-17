36e4f11198ae27d67f78b21b53dcd2e6f859b5a1
Chirag Mittal <chirag@gig4ce.com>
2021-01-18 01:06:38 +0530

    added app-giger module

---
+++ modules/app-giger/src/main/java/com/gigforce/giger_app/ui/LandingView.kt
@@ -0,0 +1,11 @@
+package com.gigforce.giger_app.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+
+class LandingView(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) :
+    LinearLayout(context, attrs, defStyleAttr, defStyleRes) {
+
+
+}
