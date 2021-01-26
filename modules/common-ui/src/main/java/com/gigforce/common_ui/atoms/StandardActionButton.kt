01fd5e274f68cadfd7d6d126205eada7cddaa91f
Chirag Mittal <chirag@gig4ce.com>
2021-01-26 16:20:43 +0530

    .

---
+++ modules/common-ui/src/main/java/com/gigforce/common_ui/atoms/StandardActionButton.kt
@@ -0,0 +1,10 @@
+package com.gigforce.common_ui.atoms
+
+import android.content.Context
+import android.util.AttributeSet
+import com.gigforce.common_ui.R
+import com.google.android.material.button.MaterialButton
+
+class StandardActionButton(context: Context,attrs: AttributeSet?) : MaterialButton(context, attrs,R.attr.stdActionButtonStyle){
+
+}
