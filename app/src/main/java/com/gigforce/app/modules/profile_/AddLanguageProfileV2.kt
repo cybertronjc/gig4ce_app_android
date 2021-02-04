ff79c559ba400e3b451c360ba444ce2baa4b73fd
Rohit Sharrrrrrrrrrma <rohit.sharma@gig4ce.com>
2021-02-04 10:15:54 +0530

    Language Autcomplete Added

--- app/src/main/java/com/gigforce/app/modules/profile_/AddLanguageProfileV2.kt
+++ app/src/main/java/com/gigforce/app/modules/profile_/AddLanguageProfileV2.kt
@@ -1,20 +1,20 @@
 package com.gigforce.app.modules.profile_
 
 import android.os.Bundle
-import android.text.Editable
 import android.text.SpannableString
 import android.text.Spanned
 import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.MultiAutoCompleteTextView
+import androidx.core.view.isVisible
 import com.gigforce.app.R
 import com.gigforce.app.core.base.BaseFragment
 import com.google.android.material.chip.Chip
 import com.google.android.material.chip.ChipDrawable
+import com.tokenautocomplete.TokenCompleteTextView
 import kotlinx.android.synthetic.main.layout_add_lang_profile_v2.*
 
 
@@ -39,23 +39,35 @@ class AddLanguageProfileV2 : BaseFragment() {
     }
 
     private fun initSearchAutoComplete() {
-        val langAdapter = ArrayAdapter<String>(
-            requireContext(),
-            android.R.layout.simple_spinner_dropdown_item,
-            resources.getStringArray(R.array.lang_array)
-        )
-        act_langs_add_lang_profile_v2.setAdapter(langAdapter)
-        act_langs_add_lang_profile_v2.setTokenizer(CommaTokenizer())
-        act_langs_add_lang_profile_v2.threshold = 1
-        act_langs_add_lang_profile_v2.onItemClickListener =
-            AdapterView.OnItemClickListener { _, _, position, _ ->
-                createRecipientChip(langAdapter.getItem(position).toString());
-                act_langs_add_lang_profile_v2.append(" ")
 
-            }
+
+        act_langs_add_lang_profile_v2.setAdapter_(
+            ArrayAdapter<String>(
+                requireContext(),
+                android.R.layout.simple_list_item_1,
+                resources.getStringArray(R.array.lang_array)
+            )
+        )
+        act_langs_add_lang_profile_v2.threshold = 1
+
+        act_langs_add_lang_profile_v2.setTokenClickStyle(TokenCompleteTextView.TokenClickStyle.Select)
+//        act_langs_add_lang_profile_v2.setAdapter(langAdapter)
+//        act_langs_add_lang_profile_v2.setTokenizer(CommaTokenizer())
+//        act_langs_add_lang_profile_v2.threshold = 1
+//        act_langs_add_lang_profile_v2.onItemClickListener =
+//                AdapterView.OnItemClickListener { _, _, position, _ ->
+//                    createRecipientChip(langAdapter.getItem(position).toString());
+//                    act_langs_add_lang_profile_v2.append(" ")
+//
+//                }
     }
 
     private fun intLangChipGroup() {
+        chip_search_add_language.setOnCheckedChangeListener { buttonView, isChecked ->
+            act_langs_add_lang_profile_v2.isVisible = isChecked
+            tv_save_add_lang.isVisible = isChecked
+            tv_cancel_add_lang.isVisible = isChecked
+        }
         listOf("English", "Hindi", "Marathi", "Punjabi").forEach { element ->
             val chip = Chip(requireContext())
             val drawable = ChipDrawable.createFromAttributes(
@@ -72,27 +84,34 @@ class AddLanguageProfileV2 : BaseFragment() {
 
     }
 
-    private fun createRecipientChip(lang: String) {
-        val chip = ChipDrawable.createFromResource(requireContext(), R.xml.standalone_chip)
-        val span = VerticalImageSpan(chip)
-        val cursorPosition: Int = act_langs_add_lang_profile_v2.selectionStart
-        val spanLength: Int = lang.length + 2
-        val text: Editable = act_langs_add_lang_profile_v2.text
-
-        chip.text = lang
-
-        chip.setBounds(0, 0, chip.intrinsicWidth, chip.intrinsicHeight)
-
-
-        text.setSpan(
-            span,
-            cursorPosition - spanLength,
-            cursorPosition,
-            Spanned.SPAN_INCLUSIVE_EXCLUSIVE
-        )
-
-
-    }
+//    private fun createRecipientChip(lang: String) {
+//        val chipView: Chip = Chip(requireContext());
+//        val chip = ChipDrawable.createFromResource(requireContext(), R.xml.standalone_chip)
+//        chipView.setChipDrawable(chip)
+//        val span = VerticalImageSpan(chip)
+//        val cursorPosition: Int = act_langs_add_lang_profile_v2.selectionStart
+//        val spanLength: Int = lang.length + 2
+//        val text: Editable = act_langs_add_lang_profile_v2.text
+//
+//        chip.text = lang
+//
+//        chip.setBounds(0, 0, chip.intrinsicWidth, chip.intrinsicHeight)
+//
+//
+//        text.setSpan(
+//            span,
+//            cursorPosition - spanLength,
+//            cursorPosition,
+//            Spanned.SPAN_INCLUSIVE_EXCLUSIVE
+//        )
+//        chipView.text = text
+//        chipView.elevation = resources.getDimensionPixelSize(R.dimen.size_20).toFloat()
+//        chipView.setOnClickListener {
+//            showToast(chipView.text.toString())
+//        }
+//
+//
+//    }
 
     class CommaTokenizer : MultiAutoCompleteTextView.Tokenizer {
         override fun findTokenStart(text: CharSequence, cursor: Int): Int {
