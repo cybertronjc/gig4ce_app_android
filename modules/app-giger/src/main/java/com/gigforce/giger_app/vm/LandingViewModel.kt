e43fba594de70b72f252c3ac58caa516c7a9b731
Amit <amitdhiman13190@gmail.com>
2021-02-04 23:45:16 +0530

    .

--- modules/app-giger/src/main/java/com/gigforce/giger_app/vm/LandingViewModel.kt
+++ modules/app-giger/src/main/java/com/gigforce/giger_app/vm/LandingViewModel.kt
@@ -21,39 +21,39 @@ class LandingViewModel @Inject constructor(
 
     var _allLandingData: LiveData<List<Any>> = allLandingData
 
-    fun getAllItems(): ArrayList<Any> {
-        val arrayList = ArrayList<Any>()
-        arrayList.add(
-            FeatureLayoutDVM(
-                R.drawable.ic_help_icon,
-                "Help",
-                getVideoItems()
-            )
-        )
+//    fun getAllItems(): ArrayList<Any> {
+//        val arrayList = ArrayList<Any>()
+//        arrayList.add(
+//            FeatureLayoutDVM(
+//                R.drawable.ic_help_icon,
+//                "Help",
+//                getVideoItems()
+//            )
+//        )
+//
+//        return arrayList
+//    }
 
-        return arrayList
-    }
-
-    private fun getVideoItems(): ArrayList<Any> {
-        val videoItemsList = ArrayList<Any>()
-        videoItemsList.add(
-            VideoItemCardDVM(
-                "https://i3.ytimg.com/vi/FbiyRe49wjY/hqdefault.jpg",
-                "[Hindi] overview",
-                true,
-                "03:32"
-            )
-        )
-        videoItemsList.add(
-            VideoItemCardDVM(
-                "https://www.youtube.com/watch?v=PnnhrIZCWeA",
-                "[Hindi] language",
-                true,
-                "00:35"
-            )
-        )
-        return videoItemsList
-    }
+//    private fun getVideoItems(): ArrayList<Any> {
+//        val videoItemsList = ArrayList<Any>()
+//        videoItemsList.add(
+//            VideoItemCardDVM(
+//                "https://i3.ytimg.com/vi/FbiyRe49wjY/hqdefault.jpg",
+//                "[Hindi] overview",
+//                true,
+//                "03:32"
+//            )
+//        )
+//        videoItemsList.add(
+//            VideoItemCardDVM(
+//                "https://www.youtube.com/watch?v=PnnhrIZCWeA",
+//                "[Hindi] language",
+//                true,
+//                "00:35"
+//            )
+//        )
+//        return videoItemsList
+//    }
 
     init {
         homeCardsFBRepository.getData().observeForever {
