e180bed310aea11d9aefed7ef516c905cfe6c341
Rohit Sharrrrrrrrrrma <rohit.sharma@gig4ce.com>
2021-02-07 17:45:18 +0530

    Profile Pic Functionality Complete

--- app/src/main/java/com/gigforce/app/modules/profile_/ProfileFragmentv2.kt
+++ app/src/main/java/com/gigforce/app/modules/profile_/ProfileFragmentv2.kt
@@ -28,9 +28,9 @@ class ProfileFragmentv2 : BaseFragment() {
     val viewModel: ProfileViewModel by activityViewModels()
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         return inflateView(R.layout.fragment_profile_v2, inflater, container)
     }
@@ -55,6 +55,8 @@ class ProfileFragmentv2 : BaseFragment() {
 
     private fun initObservers() {
         viewModel.getProfileData().observe(viewLifecycleOwner, Observer { profileData ->
+            tv_name_profile_v2.text = profileData?.name
+            tv_about_profile_v2.text = profileData?.aboutMe
             loadImage(profileData.profileAvatarName)
         })
     }
@@ -65,10 +67,10 @@ class ProfileFragmentv2 : BaseFragment() {
         ll_no_pic_profile_v2.isVisible = !isPicturePresent
         iv_profile_v2.isVisible = isPicturePresent
         val profilePicRef: StorageReference =
-                FirebaseStorage.getInstance().reference.child(PROFILE_PICTURE_FOLDER).child(path)
+            FirebaseStorage.getInstance().reference.child(PROFILE_PICTURE_FOLDER).child(path)
         GlideApp.with(this.requireContext())
-                .load(profilePicRef)
-                .into(iv_profile_v2)
+            .load(profilePicRef)
+            .into(iv_profile_v2)
     }
 
     private fun initAppBar() {
@@ -77,8 +79,8 @@ class ProfileFragmentv2 : BaseFragment() {
         act.supportActionBar?.setDisplayHomeAsUpEnabled(true)
         act.supportActionBar?.setDisplayShowHomeEnabled(true)
         toolbar.navigationIcon?.setColorFilter(
-                resources.getColor(R.color.black),
-                PorterDuff.Mode.SRC_ATOP
+            resources.getColor(R.color.black),
+            PorterDuff.Mode.SRC_ATOP
         );
         toolbar.setTitleTextColor(resources.getColor(R.color.black))
         toolbar_layout.setExpandedTitleColor(resources.getColor(R.color.black))
@@ -96,7 +98,7 @@ class ProfileFragmentv2 : BaseFragment() {
             } else if (isShow) {
                 iv_share_profile_v2.visible()
                 toolbar_layout.title =
-                        " " //careful there should a space between double quote otherwise it wont work
+                    " " //careful there should a space between double quote otherwise it wont work
                 isShow = false
             }
 
@@ -108,24 +110,66 @@ class ProfileFragmentv2 : BaseFragment() {
 
 
     private fun initViews() {
-        updateContentCardUI(add_address_profile_v2, ContentCardData(topLabel = R.string.current_address, topIcon = R.drawable.ic_location_black, contentIllustration = R.drawable.ic_address_illustration,
-                contentTitle = R.string.add_contact_address, actionText = R.string.add_now))
-        updateContentCardUI(add_education_profile_v2, ContentCardData(topLabel = R.string.education, topIcon = R.drawable.ic_education_top_profile_v2, contentIllustration = R.drawable.ic_education_profile_v2,
-                contentTitle = R.string.education, actionText = R.string.update_now, contentText = R.string.let_people_education, clickHandler = {
-            this@ProfileFragmentv2.navigate(R.id.fragment_add_education_v2)
-        }))
-        updateContentCardUI(add_experience_profile_fragment_v2, ContentCardData(topLabel = R.string.experience, topIcon = R.drawable.ic_experience_top_label_profile_v2, contentIllustration = R.drawable.ic_experience_illustration_profile_v2,
-                contentTitle = R.string.exp_matters, actionText = R.string.update_now, contentText = R.string.exp_matters))
-        updateContentCardUI(add_skills_profile_v2, ContentCardData(topLabel = R.string.skills_known, topIcon = R.drawable.ic_skills_top_icon_profile_v2, contentIllustration = R.drawable.ic_skills_profile_v2,
-                contentTitle = R.string.skills, actionText = R.string.add_now, contentText = R.string.let_people_skills))
-        updateContentCardUI(add_documents_profile_v2, ContentCardData(topLabel = R.string.documents, topIcon = R.drawable.ic_documents_top_icon_profile_v2,
+        updateContentCardUI(
+            add_address_profile_v2, ContentCardData(
+                topLabel = R.string.current_address,
+                topIcon = R.drawable.ic_location_black,
+                contentIllustration = R.drawable.ic_address_illustration,
+                contentTitle = R.string.add_contact_address,
+                actionText = R.string.add_now
+            )
+        )
+        updateContentCardUI(
+            add_education_profile_v2, ContentCardData(topLabel = R.string.education,
+                topIcon = R.drawable.ic_education_top_profile_v2,
+                contentIllustration = R.drawable.ic_education_profile_v2,
+                contentTitle = R.string.education,
+                actionText = R.string.update_now,
+                contentText = R.string.let_people_education,
+                clickHandler = {
+                    this@ProfileFragmentv2.navigate(R.id.fragment_add_education_v2)
+                })
+        )
+        updateContentCardUI(
+            add_experience_profile_fragment_v2, ContentCardData(
+                topLabel = R.string.experience,
+                topIcon = R.drawable.ic_experience_top_label_profile_v2,
+                contentIllustration = R.drawable.ic_experience_illustration_profile_v2,
+                contentTitle = R.string.exp_matters,
+                actionText = R.string.update_now,
+                contentText = R.string.exp_matters
+            )
+        )
+        updateContentCardUI(
+            add_skills_profile_v2, ContentCardData(
+                topLabel = R.string.skills_known,
+                topIcon = R.drawable.ic_skills_top_icon_profile_v2,
+                contentIllustration = R.drawable.ic_skills_profile_v2,
+                contentTitle = R.string.skills,
+                actionText = R.string.add_now,
+                contentText = R.string.let_people_skills
+            )
+        )
+        updateContentCardUI(
+            add_documents_profile_v2, ContentCardData(
+                topLabel = R.string.documents,
+                topIcon = R.drawable.ic_documents_top_icon_profile_v2,
                 contentIllustration = R.drawable.ic_documents_profile_v2,
-                contentTitle = R.string.upload_your_documents, actionText = R.string.upload_now))
-        updateContentCardUI(add_language_profile_v2, ContentCardData(topLabel = R.string.lang_known, topIcon = R.drawable.ic_language_title,
-                contentIllustration = R.drawable.ic_add_language_illustration, contentText = R.string.add_lang_known,
-                contentTitle = R.string.language, actionText = R.string.add_now, clickHandler = {
-            this@ProfileFragmentv2.navigate(R.id.fragment_add_lang_profile_v2)
-        }))
+                contentTitle = R.string.upload_your_documents,
+                actionText = R.string.upload_now
+            )
+        )
+        updateContentCardUI(
+            add_language_profile_v2, ContentCardData(topLabel = R.string.lang_known,
+                topIcon = R.drawable.ic_language_title,
+                contentIllustration = R.drawable.ic_add_language_illustration,
+                contentText = R.string.add_lang_known,
+                contentTitle = R.string.language,
+                actionText = R.string.add_now,
+                clickHandler = {
+                    this@ProfileFragmentv2.navigate(R.id.fragment_add_lang_profile_v2)
+                })
+        )
 
     }
 
@@ -133,7 +177,7 @@ class ProfileFragmentv2 : BaseFragment() {
         contentCard.topLabel = contentData.topLabel ?: R.string.empty_dash_string
         contentCard.topIcon = contentData.topIcon ?: R.drawable.ic_circle_empty
         contentCard.contentIllustration = contentData.contentIllustration
-                ?: R.drawable.ic_circle_empty
+            ?: R.drawable.ic_circle_empty
         contentCard.contentHeading = contentData.contentTitle ?: R.string.empty_dash_string
         contentCard.rightActionText = contentData.actionText ?: R.string.empty_dash_string
         contentCard.contentText = contentData.contentText ?: R.string.empty_dash_string
