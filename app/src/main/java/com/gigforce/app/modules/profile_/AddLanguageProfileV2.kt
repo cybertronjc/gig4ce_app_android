35343f740a6d0f9d8fbf40204adc6d4db1e5bc2c
Rohit Sharrrrrrrrrrma <rohit.sharma@gig4ce.com>
2021-02-10 09:17:13 +0530

    Add About me Started and margins and padding and color of profile screen verified

--- app/src/main/java/com/gigforce/app/modules/profile_/AddLanguageProfileV2.kt
+++ app/src/main/java/com/gigforce/app/modules/profile_/AddLanguageProfileV2.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.core.view.children
 import androidx.core.view.isVisible
 import com.gigforce.app.R
 import com.gigforce.app.core.base.BaseFragment
@@ -14,9 +17,9 @@ import kotlinx.android.synthetic.main.layout_add_lang_profile_v2.*
 
 class AddLanguageProfileV2 : BaseFragment() {
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         return inflateView(R.layout.layout_add_lang_profile_v2, inflater, container)
 
@@ -38,46 +41,61 @@ class AddLanguageProfileV2 : BaseFragment() {
     }
 
     private fun initSearchAutoComplete() {
-        act_langs_add_lang_profile_v2.setAdapter(
-            resources.getStringArray(R.array.lang_array).toList()
-        )
-        act_langs_add_lang_profile_v2.setHint(getString(R.string.search_lang))
-        act_langs_add_lang_profile_v2.setContentPadding(
-            paddingTop = resources.getDimensionPixelSize(
-                R.dimen.size_15
-            ), paddingBottom = resources.getDimensionPixelSize(R.dimen.size_15),
-            paddingLeft = resources.getDimensionPixelSize(R.dimen.size_5)
-        )
+        val arrayAdapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_dropdown_item_1line,resources.getStringArray(R.array.lang_array))
+        act_langs_add_lang_profile_v2.setAdapter(arrayAdapter)
+        act_langs_add_lang_profile_v2.hint = getString(R.string.search_lang)
+
+        act_langs_add_lang_profile_v2.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            run {
+                val selection = parent.getItemAtPosition(position) as String
+                val chipToAdd = getChipToAdd(selection)
+                act_langs_add_lang_profile_v2.setText("")
+                language_chip_group_add_language.children.forEach {
+                    if (it.tag == chipToAdd.tag) {
+                        showToast("Language Already Added!!!")
+                        return@run
+                    }
+                }
+                language_chip_group_add_language.addView(chipToAdd, 0)
+
+            }
+        }
+
     }
 
     private fun intLangChipGroup() {
-        chip_search_add_language.setOnCheckedChangeListener { buttonView, isChecked ->
+        chip_search_add_language.setOnCheckedChangeListener { _, isChecked ->
             act_langs_add_lang_profile_v2.isVisible = isChecked
             tv_save_add_lang.isVisible = isChecked
         }
         listOf(
-            "English",
-            "Hindi",
-            "Telugu",
-            "Kannada",
-            "Bengali",
-            "Bhojpuri",
-            "Tamil",
-            "Marathi"
+                "English",
+                "Hindi",
+                "Telugu",
+                "Kannada",
+                "Bengali",
+                "Bhojpuri",
+                "Tamil",
+                "Marathi"
         ).forEach { element ->
-            val chip = Chip(requireContext())
-            val drawable = ChipDrawable.createFromAttributes(
+            language_chip_group_add_language.addView(getChipToAdd(element), 0)
+        }
+
+    }
+
+    fun getChipToAdd(text: String): Chip {
+        val chip = Chip(requireContext())
+        val drawable = ChipDrawable.createFromAttributes(
                 requireContext(),
                 null,
                 0,
                 R.style.AppSingleChoiceChip2
-            )
-            chip.setChipDrawable(drawable)
-            chip.text = element
-            chip.chipStrokeWidth = 1f
-            language_chip_group_add_language.addView(chip, 0)
-        }
-
+        )
+        chip.setChipDrawable(drawable)
+        chip.tag=text
+        chip.text = text
+        chip.chipStrokeWidth = 1f
+        return chip
     }
 
 
