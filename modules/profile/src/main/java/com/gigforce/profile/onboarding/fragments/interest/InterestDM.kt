0246401b2799e8912cabc0bcd3990d97babadc3c
Jagdish Choudhary <cybertronjc3@gmail.com>
2021-04-17 18:54:12 +0530

    Onboarding fixes

--- modules/profile/src/main/java/com/gigforce/profile/onboarding/fragments/interest/InterestDM.kt
+++ modules/profile/src/main/java/com/gigforce/profile/onboarding/fragments/interest/InterestDM.kt
@@ -1,5 +1,5 @@
 package com.gigforce.profile.onboarding.fragments.interest
 
-data class InterestDM(val image:Int,val interestName:String,var selected : Boolean = false){
+data class InterestDM(val image: Int, val interestName:String, var selected: Boolean = false){
 
 }
