2b1cec3cea5119e21f6a68beddd805cc2cc47a78
Amit <amitdhiman13190@gmail.com>
2021-05-04 13:32:55 +0530

    gradle changes

---
+++ modules/common-image-capture-picker/build/tmp/kapt3/stubs/debug/com/gigforce/common_image_picker/CameraAndGalleryIntegrator.java
@@ -0,0 +1,82 @@
+package com.gigforce.common_image_picker;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 4, 1}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u0084\u0001\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u000b\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\u0010!\n\u0002\u0018\u0002\n\u0002\u0010 \n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0010\u0002\n\u0002\b\u0003\n\u0002\u0010\b\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0007\u0018\u0000 12\u00020\u0001:\u00011B\u000f\b\u0016\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\u0004B\u000f\b\u0016\u0012\u0006\u0010\u0005\u001a\u00020\u0006\u00a2\u0006\u0002\u0010\u0007JJ\u0010\u0011\u001a,\u0012(\u0012&\u0012\f\u0012\n \f*\u0004\u0018\u00010\u00140\u0014 \f*\u0012\u0012\f\u0012\n \f*\u0004\u0018\u00010\u00140\u0014\u0018\u00010\u00150\u00130\u00122\u0006\u0010\u0016\u001a\u00020\u00172\u0006\u0010\u0018\u001a\u00020\u00192\u0006\u0010\u001a\u001a\u00020\u001bH\u0002J\b\u0010\u001c\u001a\u00020\u001dH\u0002J\u0010\u0010\u001e\u001a\u00020\u001f2\u0006\u0010 \u001a\u00020\u0019H\u0002J\b\u0010!\u001a\u00020\"H\u0016J\b\u0010#\u001a\u00020\"H\u0016J\u0006\u0010\r\u001a\u00020\"J0\u0010$\u001a\u00020\"2\u0006\u0010%\u001a\u00020&2\u0006\u0010\'\u001a\u00020&2\b\u0010(\u001a\u0004\u0018\u00010)2\u0006\u0010*\u001a\u00020+2\u0006\u0010\u0016\u001a\u00020\u0017J\u0018\u0010,\u001a\u00020\"2\u0006\u0010\u0016\u001a\u00020\u00172\u0006\u0010\u0018\u001a\u00020\u0019H\u0002J\u0006\u0010-\u001a\u00020\"J\u0006\u0010.\u001a\u00020\"J\u0006\u0010/\u001a\u00020\"J\u0018\u00100\u001a\u00020\"2\u0006\u0010 \u001a\u00020\u00192\u0006\u0010*\u001a\u00020+H\u0002R\u000e\u0010\u0002\u001a\u00020\u0003X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u000e\u0010\b\u001a\u00020\tX\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u0016\u0010\n\u001a\n \f*\u0004\u0018\u00010\u000b0\u000bX\u0082\u0004\u00a2\u0006\u0002\n\u0000R\u0010\u0010\u0005\u001a\u0004\u0018\u00010\u0006X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u000e\u0010\r\u001a\u00020\u000eX\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u0016\u0010\u000f\u001a\n \f*\u0004\u0018\u00010\u00100\u0010X\u0082\u0004\u00a2\u0006\u0002\n\u0000\u00a8\u00062"}, d2 = {"Lcom/gigforce/common_image_picker/CameraAndGalleryIntegrator;", "Lcom/gigforce/common_image_picker/ClickOrSelectImageBottomSheet$OnPickOrCaptureImageClickListener;", "activity", "Landroidx/fragment/app/FragmentActivity;", "(Landroidx/fragment/app/FragmentActivity;)V", "fragment", "Landroidx/fragment/app/Fragment;", "(Landroidx/fragment/app/Fragment;)V", "context", "Landroid/content/Context;", "detector", "Lcom/google/firebase/ml/vision/face/FirebaseVisionFaceDetector;", "kotlin.jvm.PlatformType", "openFrontCamera", "", "options", "Lcom/google/firebase/ml/vision/face/FirebaseVisionFaceDetectorOptions;", "detectFacesAndReturnResult", "Lcom/google/android/gms/tasks/Task;", "", "Lcom/google/firebase/ml/vision/face/FirebaseVisionFace;", "", "callback", "Lcom/gigforce/common_image_picker/ImageCropCallback;", "outputFileUri", "Landroid/net/Uri;", "firebaseVisionImage", "Lcom/google/firebase/ml/vision/common/FirebaseVisionImage;", "getCropOptions", "Lcom/yalantis/ucrop/UCrop$Options;", "getImageDimensions", "Landroid/util/Size;", "uri", "onClickPictureThroughCameraClicked", "", "onPickImageThroughCameraClicked", "parseResults", "requestCode", "", "resultCode", "data", "Landroid/content/Intent;", "imageCropOptions", "Lcom/gigforce/common_image_picker/ImageCropOptions;", "returnFileImage", "showCameraAndGalleryBottomSheet", "startCameraForCapturing", "startGalleryForPicking", "startImageCropper", "Companion", "common-image-capture-picker_debug"})
+public final class CameraAndGalleryIntegrator implements com.gigforce.common_image_picker.ClickOrSelectImageBottomSheet.OnPickOrCaptureImageClickListener {
+    private androidx.fragment.app.FragmentActivity activity;
+    private android.content.Context context;
+    private androidx.fragment.app.Fragment fragment;
+    private boolean openFrontCamera = false;
+    private final com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions options = null;
+    private final com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector detector = null;
+    public static final int REQUEST_CAPTURE_IMAGE = 101;
+    public static final int REQUEST_PICK_IMAGE = 102;
+    public static final int REQUEST_CROP = 69;
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String EXTENSION = ".jpg";
+    @org.jetbrains.annotations.NotNull()
+    public static final com.gigforce.common_image_picker.CameraAndGalleryIntegrator.Companion Companion = null;
+    
+    public final void openFrontCamera() {
+    }
+    
+    public final void showCameraAndGalleryBottomSheet() {
+    }
+    
+    public final void startCameraForCapturing() {
+    }
+    
+    public final void startGalleryForPicking() {
+    }
+    
+    public final void parseResults(int requestCode, int resultCode, @org.jetbrains.annotations.Nullable()
+    android.content.Intent data, @org.jetbrains.annotations.NotNull()
+    com.gigforce.common_image_picker.ImageCropOptions imageCropOptions, @org.jetbrains.annotations.NotNull()
+    com.gigforce.common_image_picker.ImageCropCallback callback) {
+    }
+    
+    private final void returnFileImage(com.gigforce.common_image_picker.ImageCropCallback callback, android.net.Uri outputFileUri) {
+    }
+    
+    private final void startImageCropper(android.net.Uri uri, com.gigforce.common_image_picker.ImageCropOptions imageCropOptions) {
+    }
+    
+    private final android.util.Size getImageDimensions(android.net.Uri uri) {
+        return null;
+    }
+    
+    private final com.yalantis.ucrop.UCrop.Options getCropOptions() {
+        return null;
+    }
+    
+    private final com.google.android.gms.tasks.Task<java.util.List<com.google.firebase.ml.vision.face.FirebaseVisionFace>> detectFacesAndReturnResult(com.gigforce.common_image_picker.ImageCropCallback callback, android.net.Uri outputFileUri, com.google.firebase.ml.vision.common.FirebaseVisionImage firebaseVisionImage) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public void onClickPictureThroughCameraClicked() {
+    }
+    
+    @java.lang.Override()
+    public void onPickImageThroughCameraClicked() {
+    }
+    
+    public CameraAndGalleryIntegrator(@org.jetbrains.annotations.NotNull()
+    androidx.fragment.app.FragmentActivity activity) {
+        super();
+    }
+    
+    public CameraAndGalleryIntegrator(@org.jetbrains.annotations.NotNull()
+    androidx.fragment.app.Fragment fragment) {
+        super();
+    }
+    
+    @kotlin.Metadata(mv = {1, 4, 1}, bv = {1, 0, 3}, k = 1, d1 = {"\u0000\u001a\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0000\n\u0002\u0010\b\n\u0002\b\u0003\b\u0086\u0003\u0018\u00002\u00020\u0001B\u0007\b\u0002\u00a2\u0006\u0002\u0010\u0002R\u000e\u0010\u0003\u001a\u00020\u0004X\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0005\u001a\u00020\u0006X\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010\u0007\u001a\u00020\u0006X\u0086T\u00a2\u0006\u0002\n\u0000R\u000e\u0010\b\u001a\u00020\u0006X\u0086T\u00a2\u0006\u0002\n\u0000\u00a8\u0006\t"}, d2 = {"Lcom/gigforce/common_image_picker/CameraAndGalleryIntegrator$Companion;", "", "()V", "EXTENSION", "", "REQUEST_CAPTURE_IMAGE", "", "REQUEST_CROP", "REQUEST_PICK_IMAGE", "common-image-capture-picker_debug"})
+    public static final class Companion {
+        
+        private Companion() {
+            super();
+        }
+    }
+}
