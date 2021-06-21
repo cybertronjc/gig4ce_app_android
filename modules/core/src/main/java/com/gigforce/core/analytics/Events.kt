7800121fef7655239d8a69da8077bdcc3c338770
Amit <amitdhiman13190@gmail.com>
2021-06-21 15:11:33 +0530

    login signin/signup event revamp

--- modules/core/src/main/java/com/gigforce/core/analytics/Events.kt
+++ modules/core/src/main/java/com/gigforce/core/analytics/Events.kt
@@ -24,9 +24,10 @@ object AuthEvents{
     const val SIGN_RESEND_OTP = "signup_resend_otp"
     const val LOGIN_RESEND_OTP = "login_resend_otp"
     const val SIGN_SUCCESS = "signup_success"
+
     const val LOGIN_STARTED = "login_started"
     const val LOGIN_SUCCESS = "login_success"
-    const val SIGN_OUT_SUCCESS = "signout_success"
 
+    const val SIGN_OUT_SUCCESS = "signout_success"
     const val SIGN_UP_OTP_SCREEN_LOADED = "verify_otp_loaded"
 }
