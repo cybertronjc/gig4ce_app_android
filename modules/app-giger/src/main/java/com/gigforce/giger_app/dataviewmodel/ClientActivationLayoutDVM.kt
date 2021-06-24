ed44c54fde1d099c528619c33fd2cc36ea4d5447
Amit <amitdhiman13190@gmail.com>
2021-06-24 14:57:22 +0530

    component development done : one raised bug pending

--- modules/app-giger/src/main/java/com/gigforce/giger_app/dataviewmodel/ClientActivationLayoutDVM.kt
+++
@@ -1,7 +0,0 @@
-package com.gigforce.giger_app.dataviewmodel
-
-import com.gigforce.core.SimpleDVM
-import com.gigforce.common_ui.viewconfigs.AppModuleLevelViewTypes
-
-class ClientActivationLayoutDVM(val type:String = ""): SimpleDVM(AppModuleLevelViewTypes.VIEW_CLIENT_ACTIVATION_SECTION) {
-}
--- modules/client-activation/src/main/java/com/gigforce/client_activation/ui/ClientActivationLayoutComponent.kt
+++ modules/client-activation/src/main/java/com/gigforce/client_activation/ui/ClientActivationLayoutComponent.kt
@@ -2,6 +2,7 @@ package com.gigforce.client_activation.ui
 
 import android.content.Context
 import android.util.AttributeSet
+import com.gigforce.client_activation.client_activation.dataviewmodel.ClientActivationLayoutDVM
 import com.gigforce.client_activation.repo.IClientActivationDataRepository
 import com.gigforce.common_ui.components.cells.FeatureLayoutComponent
 import com.gigforce.common_ui.viewdatamodels.FeatureLayoutDVM
@@ -14,9 +15,12 @@ class ClientActivationLayoutComponent(context: Context, attrs: AttributeSet?) :
     @Inject
     lateinit var repository: IClientActivationDataRepository
 
-    init {
-        repository.getData().observeForever {
-            bind(FeatureLayoutDVM("", "Explore Gigs", it))
+
+    override fun bind(data: Any?) {
+        if(data is ClientActivationLayoutDVM){
+            repository.getData().observeForever {
+                super.bind(FeatureLayoutDVM(data.image,data.title,it))
+            }
         }
     }
 
