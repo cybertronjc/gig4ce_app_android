c1e4f6c61e5c4691f9ecf51e56c79bfdcd7d2126
Himanshu <him.khati@gmail.com>
2021-07-13 17:02:30 +0530

    toolbar color name changed to Status bar

--- modules/lead-management/src/main/java/com/gigforce/lead_management/ui/joining_list/JoiningListFragment.kt
+++ modules/lead-management/src/main/java/com/gigforce/lead_management/ui/joining_list/JoiningListFragment.kt
@@ -7,7 +7,7 @@ import com.gigforce.lead_management.databinding.FragmentJoiningListBinding
 class JoiningListFragment : BaseFragment2<FragmentJoiningListBinding>(
     fragmentName = "JoiningListFragment",
     layoutId = R.layout.fragment_joining_list,
-    toolbarColor = R.color.lipstick_2
+    statusBarColor = R.color.lipstick_2
 ) {
 
     override fun viewCreated(viewBinding: FragmentJoiningListBinding) {
