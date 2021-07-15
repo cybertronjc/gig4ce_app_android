4295aa25035ee30c976fec1e663f2272e17c093b
Jagdish Choudhary <cybertronjc3@gmail.com>
2021-07-15 19:56:46 +0530

    Select gig application to activate view completed

---
+++ modules/common-ui/src/main/java/com/gigforce/common_ui/viewdatamodels/GigerProfileCardDVM.kt
@@ -0,0 +1,4 @@
+package com.gigforce.common_ui.viewdatamodels
+
+class GigerProfileCardDVM(val gigerImg: String, val name : String,  var number : String, var jobProfileName: String, val jobProfileLogo: String) {
+}
