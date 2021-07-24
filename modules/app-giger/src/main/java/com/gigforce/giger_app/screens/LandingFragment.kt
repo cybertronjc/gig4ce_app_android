1909ed368ac89fd9dc095ea2cdb9c5ccd1e6144d
Amit <amitdhiman13190@gmail.com>
2021-07-24 18:41:46 +0530

    location bottom sheet integrate on landing screen and Calendar screen

--- modules/app-giger/src/main/java/com/gigforce/giger_app/screens/LandingFragment.kt
+++ modules/app-giger/src/main/java/com/gigforce/giger_app/screens/LandingFragment.kt
@@ -3,11 +3,13 @@ package com.gigforce.giger_app.screens
 import android.app.Dialog
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Location
 import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -16,6 +18,7 @@ import com.gigforce.common_ui.AppDialogsInterface
 import com.gigforce.common_ui.ConfirmationDialogOnClickListener
 import com.gigforce.common_ui.configrepository.ConfigRepository
 import com.gigforce.common_ui.deviceInfo_permission.DeviceInfoGatherer
+import com.gigforce.common_ui.utils.LocationUpdates
 import com.gigforce.core.navigation.INavigation
 import com.gigforce.giger_app.R
 import com.gigforce.giger_app.vm.LandingViewModel
@@ -27,11 +30,16 @@ import javax.inject.Inject
 
 
 @AndroidEntryPoint
-class LandingFragment : Fragment() {
+class LandingFragment : Fragment(),
+    LocationUpdates.LocationUpdateCallbacks {
     val viewModel: LandingViewModel by viewModels()
     @Inject lateinit var navigation: INavigation
     @Inject
     lateinit var appDialogsInterface: AppDialogsInterface
+
+    var locationUpdates: LocationUpdates? = LocationUpdates()
+    var location: Location? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -148,6 +156,22 @@ class LandingFragment : Fragment() {
                 null
         )
         )
+
+        locationUpdates?.startUpdates(requireActivity() as AppCompatActivity)
+        locationUpdates?.setLocationUpdateCallbacks(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        locationUpdates?.stopLocationUpdates(requireActivity())
+    }
+
+    override fun locationReceiver(location: Location?) {
+
+    }
+
+    override fun lastLocationReceiver(location: Location?) {
+
     }
 
 }
--- modules/common-ui/src/main/java/com/gigforce/common_ui/utils/LocationUpdates.kt
+++ modules/common-ui/src/main/java/com/gigforce/common_ui/utils/LocationUpdates.kt
@@ -11,7 +11,6 @@ import android.location.LocationManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import com.gigforce.common_ui.utils.BsLocationAccess
 import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
 
