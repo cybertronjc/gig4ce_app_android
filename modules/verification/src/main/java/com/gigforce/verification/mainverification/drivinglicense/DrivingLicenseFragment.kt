7dcef7c7b8578588fbbe6aa6c917948a109b81ef
Amit <amitdhiman13190@gmail.com>
2021-08-05 11:27:27 +0530

    crash resolve due to text change

--- modules/verification/src/main/java/com/gigforce/verification/mainverification/drivinglicense/DrivingLicenseFragment.kt
+++ modules/verification/src/main/java/com/gigforce/verification/mainverification/drivinglicense/DrivingLicenseFragment.kt
@@ -214,11 +214,9 @@ class DrivingLicenseFragment : Fragment(),
         }
 
         override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            TODO("Not yet implemented")
         }
 
         override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            TODO("Not yet implemented")
         }
 
     }
