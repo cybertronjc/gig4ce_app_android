0eb72a88b3f6b5fa56764926cf2cc07389e2b4e6
Himanshu <him.khati@gmail.com>
2021-09-03 15:02:31 +0530

    Weekend Data issue

--- modules/user_preferences/src/main/java/com/gigforce/user_preferences/daytime/weekend/WeekEndFragment.kt
+++ modules/user_preferences/src/main/java/com/gigforce/user_preferences/daytime/weekend/WeekEndFragment.kt
@@ -59,6 +59,7 @@ class WeekEndFragment : Fragment() {
                 initializeViews()
             })
         viewModel.getConfiguration()
+        viewModel.getAllData()
     }
 
     private fun initializeViews() {
