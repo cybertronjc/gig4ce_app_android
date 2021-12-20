21cc4e653e56847f1bc5e9c0a4740dd547bd2fa6
Jagdish Choudhary <cybertronjc3@gmail.com>
2021-12-20 17:14:06 +0530

    Chat mixpanel initial commit

---
+++ modules/feature-chat/src/main/java/com/gigforce/modules/feature_chat/analytics/CommunityEvents.kt
@@ -0,0 +1,21 @@
+package com.gigforce.modules.feature_chat.analytics
+
+object CommunityEvents {
+
+    const val EVENT_CHAT_SESSION_STARTED = "Chat_session_started"
+    const val EVENT_CHAT_SESSION_ENDED = "Chat_session_ended"
+    const val EVENT_CHAT_LIST_SCREEN = "Chat_list_screen"
+    const val EVENT_CHAT_MARKED_READ = "Marked_as_read"
+    const val EVENT_USER_UPDATED_AGE_GROUP = "updated_age_group"
+    const val EVENT_USER_UPDATED_HIGHEST_QUALIFICATION = "updated_high_qualification"
+    const val EVENT_USER_UPDATED_PREF_LOCATION = "updated_pref_location"
+    const val EVENT_USER_ASSETS_SELECTED = "assets_selected"
+    const val EVENT_USER_UPDATED_INTREST = "updated_interest"
+    const val EVENT_USER_EXPLOYMENT_PREFERENCE_SELECTED = "employment_preference_selected"
+    const val EVENT_USER_UPDATED_EXPERIENCE = "updated_experience"
+    const val EVENT_USER_CURRENT_JOB_STATUS_SELECTED = "current_job_status_selected"
+    const val EVENT_USER_TIME_PREFERENCE_SELECTED = "time_slot_preference_selected"
+    const val EVENT_USER_WORK_DAYS_PREFERRED_SELECTED = "workdays_preference_selected"
+    const val EVENT_USER_TIME_JOB_PREFERENCE_SELECTED = "job_preference_selected"
+    const val EVENT_USER_UPLOADED_PROFILE_PHOTO = "profile_photo_uploaded"
+}
