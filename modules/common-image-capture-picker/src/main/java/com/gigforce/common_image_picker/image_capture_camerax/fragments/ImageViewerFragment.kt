6e700280ac8bf1395f56a7c778807d07a11c4421
Jagdish Choudhary <jagdish.choudhary@gig4ce.com>
2022-03-09 15:27:22 +0530

    Pop up when no faces detected in Giger attendance

--- modules/common-image-capture-picker/src/main/java/com/gigforce/common_image_picker/image_capture_camerax/fragments/ImageViewerFragment.kt
+++ modules/common-image-capture-picker/src/main/java/com/gigforce/common_image_picker/image_capture_camerax/fragments/ImageViewerFragment.kt
@@ -180,11 +180,25 @@ class ImageViewerFragment : Fragment() {
                     is ImageViewerViewState.ErrorWhileFaceDetection -> {
 
                         progressBar.visibility = View.GONE
+//                        MaterialAlertDialogBuilder(requireContext())
+//                            .setTitle("Unable to detect face")
+//                            .setMessage(getString(R.string.something_seems_off_common) )
+//                            .setPositiveButton(getString(R.string.okay_common)) { _, _ ->
+//                                sharedCameraViewModel.clickedImageDiscarded()
+//                            }
+//                            .show()
                         MaterialAlertDialogBuilder(requireContext())
                             .setTitle("Unable to detect face")
-                            .setMessage(getString(R.string.something_seems_off_common) )
-                            .setPositiveButton(getString(R.string.okay_common)) { _, _ ->
+                            .setMessage(getString(R.string.no_face_detected_details_common) )
+                            .setPositiveButton(getString(R.string.try_again_common)) { _, _ ->
                                 sharedCameraViewModel.clickedImageDiscarded()
+                            }
+                            .setNegativeButton(getString(R.string.submit_common)) {dialog, _ ->
+                                viewModel.faceNotDetectedStillUploadImage(
+                                    requireContext().applicationContext,
+                                    image,
+                                    parentDirectoryNameInFirebaseStorage
+                                )
                             }
                             .show()
                     }
