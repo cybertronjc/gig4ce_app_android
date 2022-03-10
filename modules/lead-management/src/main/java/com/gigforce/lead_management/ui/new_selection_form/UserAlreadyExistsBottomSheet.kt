b9f7b6a49462fb70e86644bc603eac8a80caa6d1
Amit <amitdhiman13190@gmail.com>
2022-03-10 23:03:52 +0530

    user already exists error message

---
+++ modules/lead-management/src/main/java/com/gigforce/lead_management/ui/new_selection_form/UserAlreadyExistsBottomSheet.kt
@@ -0,0 +1,58 @@
+package com.gigforce.lead_management.ui.new_selection_form
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.gigforce.common_ui.ext.pushOnclickListener
+import com.gigforce.core.navigation.INavigation
+import com.gigforce.lead_management.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.layout_user_already_exists_bs.*
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class UserAlreadyExistsBottomSheet : BottomSheetDialogFragment() {
+
+    @Inject
+    lateinit var navigation: INavigation
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.BSDialogStyle)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.layout_user_already_exists_bs, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getIntentData()
+        initializeAll()
+        listeners()
+    }
+
+    private fun listeners() {
+        okay_btn.pushOnclickListener{
+            dismiss()
+        }
+    }
+
+    private fun initializeAll() {
+        if (errorMessage.isNotBlank())
+            subheading.text = errorMessage
+    }
+
+    var errorMessage = ""
+    private fun getIntentData() {
+        arguments?.let {
+            errorMessage = it.getString("message") ?: ""
+        }
+    }
+
+}
