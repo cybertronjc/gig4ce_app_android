4d7f654f144cf159bbc8415c9fd677459b535efb
Jagdish Choudhary <jagdish.choudhary@gig4ce.com>
2022-03-11 12:41:35 +0530

    Dynamic initial commit

---
+++ modules/common-ui/src/main/java/com/gigforce/common_ui/dynamic_fields/types/DynamicMultiSelectDropDown.kt
@@ -0,0 +1,200 @@
+package com.gigforce.common_ui.dynamic_fields.types
+
+import android.R
+import android.content.Context
+import android.os.Parcelable
+import android.text.SpannedString
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import androidx.core.text.bold
+import androidx.core.text.buildSpannedString
+import com.gigforce.common_ui.databinding.LayoutDynamicMultiSelectDropDownBinding
+import com.gigforce.common_ui.dynamic_fields.DynamicFieldView
+import com.gigforce.common_ui.dynamic_fields.data.DataFromDynamicInputField
+import com.gigforce.common_ui.dynamic_fields.data.DynamicField
+import com.gigforce.common_ui.dynamic_fields.data.DynamicFieldData
+import com.gigforce.common_ui.dynamic_fields.data.FieldTypes
+import com.gigforce.common_ui.ext.addMandatorySymbolToTextEnd
+import com.gigforce.common_ui.ext.addOnItemSelectedListener
+import com.gigforce.core.extensions.gone
+import com.gigforce.core.extensions.visible
+import kotlinx.android.parcel.Parcelize
+
+class DynamicMultiSelectDropDown(
+    context: Context,
+    attrs: AttributeSet?
+) : LinearLayout(
+    context,
+    attrs
+), DynamicFieldView {
+    private var viewBinding: LayoutDynamicMultiSelectDropDownBinding
+    private lateinit var viewData: DynamicField
+    private var editTextString: String = ""
+
+    override val fieldType: String
+        get() = FieldTypes.DROP_DOWN
+
+    init {
+        this.layoutParams =
+            LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+        viewBinding = LayoutDynamicMultiSelectDropDownBinding.inflate(
+            LayoutInflater.from(context),
+            this,
+            true
+        )
+    }
+
+    override fun bind(
+        fieldDetails: DynamicField
+    ) {
+        viewData = fieldDetails
+        tag = id //setting id of dynamic view as view tag to identify layout at runtime
+
+        setTitle(fieldDetails.title)
+        settingFieldAsOptionalOrMandatory(fieldDetails)
+
+        if(fieldDetails.data != null) {
+            setDataOnSpinner(
+                fieldDetails.title,
+                fieldDetails.data
+            )
+        }
+    }
+
+    private fun setDataOnSpinner(
+        title: String?,
+        dataToShow: List<DynamicFieldData>
+    ) = viewBinding.spinner.apply {
+
+        val spinnerAdapter: ArrayAdapter<DynamicFieldData> = ArrayAdapter<DynamicFieldData>(
+            context,
+            R.layout.simple_spinner_dropdown_item,
+            dataToShow.toMutableList().apply {
+                add(
+                    0, DynamicFieldData(
+                        id = "",
+                        value = "Select $title"
+                    )
+                )
+            }
+        )
+
+        adapter = spinnerAdapter
+
+        this.onItemClickListener = object : AdapterView.OnItemClickListener {
+            override fun onItemClick(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+
+            }
+
+        }
+    }
+
+    private fun setTitle(title: String?) {
+        viewBinding.titleTextview.text = title
+    }
+
+    private fun settingFieldAsOptionalOrMandatory(fieldDetails: DynamicField) {
+        if (fieldDetails.mandatory) {
+            viewBinding.optionalTextview.gone()
+            viewBinding.titleTextview.addMandatorySymbolToTextEnd()
+        } else {
+            viewBinding.optionalTextview.visible()
+        }
+    }
+
+//    override fun onSaveInstanceState(): Parcelable? {
+//        val superState = super.onSaveInstanceState()
+//        return StateSavingObject(superState, editTextString)
+//    }
+//
+//    override fun onRestoreInstanceState(state: Parcelable?) {
+//        val myState = state as? StateSavingObject
+//        super.onRestoreInstanceState(myState?.superState ?: state)
+//
+//        editTextString = myState?.editTextText ?: ""
+//
+//        if (editTextString.isNotBlank())
+//            viewBinding.editText.setText(editTextString)
+//    }
+
+    override fun isEnteredOrSelectedDataValid(): Boolean {
+
+        return if (viewData.mandatory) {
+            viewBinding.spinner.childCount != 0 && viewBinding.spinner.selectedItemPosition != 0
+        } else {
+            true
+        }
+    }
+
+    override fun setError(
+        error: SpannedString
+    ) {
+        viewBinding.errorLayout.root.visible()
+        viewBinding.errorLayout.errorTextview.text = error
+    }
+
+    override fun removeError() {
+        viewBinding.errorLayout.errorTextview.text = null
+        viewBinding.errorLayout.root.gone()
+    }
+
+    override fun validateDataAndReturnDataElseNull(): DataFromDynamicInputField? {
+        return if (isEnteredOrSelectedDataValid()) {
+            removeError()
+            getUserEnteredOrSelectedData()
+        } else {
+            checkDataAndSetError()
+            null
+        }
+    }
+
+    private fun getUserEnteredOrSelectedData(): DataFromDynamicInputField {
+
+        val valueSelected = if(viewBinding.spinner.childCount != 0 && viewBinding.spinner.selectedItemPosition != 0){
+            viewBinding.spinner.selectedItem as DynamicFieldData
+        } else{
+            DynamicFieldData()
+        }
+
+        return DataFromDynamicInputField(
+            id = viewData.id,
+            title = viewData.title,
+            valueId = valueSelected.id,
+            value = valueSelected.value,
+            fieldType = FieldTypes.DROP_DOWN
+        )
+    }
+
+    private fun checkDataAndSetError() {
+        if (viewData.mandatory) {
+
+            if (!isEnteredOrSelectedDataValid()) {
+
+                setError(buildSpannedString {
+                    bold {
+                        append(
+                            resources.getString(com.gigforce.common_ui.R.string.common_note_with_colon)
+                        )
+                    }
+                    append(" Please select ${viewData.title}")
+                })
+            } else {
+                removeError()
+            }
+        }
+    }
+
+    @Parcelize
+    class StateSavingObject(
+        val parcelable: Parcelable?,
+        val editTextText: String
+    ) : View.BaseSavedState(parcelable)
+}
