e6258a1263c018ed22f4cf8a393c215fc26728db
Amit <amitdhiman13190@gmail.com>
2022-03-31 19:21:55 +0530

    help section completed

--- modules/lead-management/src/main/java/com/gigforce/lead_management/ui/joining_list_2/JoiningList2Fragment.kt
+++ modules/lead-management/src/main/java/com/gigforce/lead_management/ui/joining_list_2/JoiningList2Fragment.kt
@@ -261,6 +261,7 @@ class JoiningList2Fragment : BaseFragment2<FragmentJoiningList2Binding>(
         makeBackgroundMoreRound()
         makeTitleBold()
         makeHelpVisible(true)
+        listeners()
         setBackButtonListener(View.OnClickListener {
             activity?.onBackPressed()
         })
@@ -278,6 +279,12 @@ class JoiningList2Fragment : BaseFragment2<FragmentJoiningList2Binding>(
             navigation.navigateTo("LeadMgmt/joiningFilter", bundleOf(
                 StringConstants.INTENT_FILTER_DAYS_NUMBER.value to filterDaysFM
             ))
+        }
+    }
+
+    private fun listeners() {
+        viewBinding.appBarComp.helpImageButton.setOnClickListener{
+            navigation.navigateTo("HelpSectionFragment")
         }
     }
 
