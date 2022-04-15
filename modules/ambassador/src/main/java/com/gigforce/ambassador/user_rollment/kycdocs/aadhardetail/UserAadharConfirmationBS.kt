eed9f95045550e34bce67cacc976ed358eb8bfad
Amit Dhiman <amit.dhiman@gig4ce.com>
2022-04-15 15:40:08 +0530

    aadhar edit done

--- modules/ambassador/src/main/java/com/gigforce/ambassador/user_rollment/kycdocs/aadhardetail/UserAadharConfirmationBS.kt
+++ modules/ambassador/src/main/java/com/gigforce/ambassador/user_rollment/kycdocs/aadhardetail/UserAadharConfirmationBS.kt
@@ -1,4 +1,4 @@
-package com.gigforce.verification.mainverification.aadhardetail
+package com.gigforce.ambassador.user_rollment.kycdocs.aadhardetail
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,16 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
+import com.gigforce.ambassador.R
 import com.gigforce.common_ui.ext.pushOnclickListener
-import com.gigforce.verification.R
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.aadhar_confirmation_bs_layout.*
+import kotlinx.android.synthetic.main.user_aadhar_confirmation_bs_layout.*
 
 @AndroidEntryPoint
-class AadharConfirmationBS : BottomSheetDialogFragment() {
+class UserAadharConfirmationBS : BottomSheetDialogFragment() {
 
-    private val viewModel: AadharDetailInfoViewModel by activityViewModels()
+    private val viewModel: UserAadharDetailInfoViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +26,7 @@ class AadharConfirmationBS : BottomSheetDialogFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.aadhar_confirmation_bs_layout, container, false)
+        return inflater.inflate(R.layout.user_aadhar_confirmation_bs_layout, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
